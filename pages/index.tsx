19c55149ad46d985a00a234453b99d06e47a5720
MINJE-98 <jmj012100@gmail.com>
2022-04-13 21:28:29 +0900

    feat: index page에 TextInput 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import type { NextPage } from "next";
+
 import {
   AButtonCancel,
   AButtonConfirm,
@@ -5,38 +7,48 @@ import {
   AButtonTag,
   AButtonToolbar,
 } from "@Atoms/Button";
-import type { NextPage } from "next";
+import ATextInput from "@Atoms/TextInput";
+
 const Home: NextPage = () => {
   const handleLogin = () => {
-    alert("로그인!");
+    return;
   };
   const handleConfirm = () => {
-    alert("확인!");
+    return;
   };
   const handleCancel = () => {
-    alert("취소!");
+    return;
   };
   const handleToolbar = () => {
-    alert("툴바!");
+    return;
   };
   const handleTag = () => {
-    alert("테그!");
+    return;
   };
   return (
     <div className="p-10 flex justify-between">
-      <AButtonLogin
-        onClick={handleLogin}
-        styles="w-52 h-12"
-        text="Login with Github"
-      />
-      <AButtonConfirm
-        onClick={handleConfirm}
-        styles="w-52 h-12"
-        text="confirm"
-      />
-      <AButtonCancel onClick={handleCancel} styles="w-52 h-12" text="Cancel" />
-      <AButtonToolbar onClick={handleToolbar} styles="w-10 h-10" text="H1" />
-      <AButtonTag onClick={handleTag} styles="w-52 h-12" text="# NextJS" />
+      <p className="p-5">
+        <AButtonLogin
+          onClick={handleLogin}
+          styles="w-52 h-12"
+          text="Login with Github"
+        />
+        <AButtonConfirm
+          onClick={handleConfirm}
+          styles="w-52 h-12"
+          text="confirm"
+        />
+        <AButtonCancel
+          onClick={handleCancel}
+          styles="w-52 h-12"
+          text="Cancel"
+        />
+        <AButtonToolbar onClick={handleToolbar} styles="w-10 h-10" text="H1" />
+        <AButtonTag onClick={handleTag} styles="w-52 h-12" text="# NextJS" />
+      </p>
+      <p>
+        <ATextInput placeholder="TextInput" styles="text-xl" />
+      </p>
     </div>
   );
 };
