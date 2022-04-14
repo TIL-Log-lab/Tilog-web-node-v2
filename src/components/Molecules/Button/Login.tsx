53b0538070c9edce6520cb2dc34d0cefce9bfaab
MINJE-98 <jmj012100@gmail.com>
2022-04-14 23:17:21 +0900

    fix: Button안에 LoginButton이 있도록 변경

--- src/components/Molecules/Button/Login.tsx
+++ src/components/Molecules/Button/Login.tsx
@@ -1,12 +1,12 @@
-interface MLoginButtonProps {
+interface MButtonLoginProps {
   onClick?: () => void;
 }
 
-const MLoginButton = ({ onClick }: MLoginButtonProps) => {
+const MButtonLogin = ({ onClick }: MButtonLoginProps) => {
   return (
     <button onClick={onClick} className={`rounded bg-black w-72 h-12`}>
       <p className="text-sm text-white">Login With Github</p>
     </button>
   );
 };
-export default MLoginButton;
+export default MButtonLogin;
