2666054b5565b76603780aff7ece6d32121b0d7c
MINJE-98 <jmj012100@gmail.com>
2022-04-14 22:41:39 +0900

    feat: 템플릿 추가

---
+++ src/components/Template/Login/index.tsx
@@ -0,0 +1,10 @@
+import OLogin from "src/components/Organisms/Login";
+
+const TLogin = () => {
+  return (
+    <div className="bg-white rounded p-14 dark:bg-neutral-900">
+      <OLogin />
+    </div>
+  );
+};
+export default TLogin;
