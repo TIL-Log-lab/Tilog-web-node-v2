1092619acf0882a41b9af84bed082c6dc5e4afa4
MINJE-98 <jmj012100@gmail.com>
2022-04-14 22:10:29 +0900

    feat: 스타일 추가 및 스타일 주입 코드 추가

--- src/components/Molecules/TitleAndDesc.tsx
+++ src/components/Molecules/TitleAndDesc.tsx
@@ -1,13 +1,17 @@
 interface MTitleAndDescProps {
+  isCenter: boolean;
+  space?: string;
   title: string;
   desc: string;
 }
 
-const MTitleAndDesc = ({ title, desc }: MTitleAndDescProps) => {
+const MTitleAndDesc = ({ title, desc, isCenter }: MTitleAndDescProps) => {
   return (
-    <div className="flex flex-col">
-      <h2>{title}</h2>
-      <p>{desc}</p>
+    <div
+      className={`flex flex-col space-y-3 ${isCenter ? "items-center" : ""}`}
+    >
+      <h1 className="font-bold">{title}</h1>
+      <p className="text-base font-normal">{desc}</p>
     </div>
   );
 };
