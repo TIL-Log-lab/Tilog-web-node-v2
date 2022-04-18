d65165ba2507ad961936225461074bc04f367758
MINJE-98 <jmj012100@gmail.com>
2022-04-18 15:23:54 +0900

    feat: axios error 타입 지정

--- api/getUserInfo.ts
+++ api/getUserInfo.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { tilogApi } from "api/core";
+import { isExceptionMessageInterface } from "interface/MessageError";
 export const getUserInfo = () => {
   return new Promise<string>(async (resolve, reject) => {
     try {
@@ -9,7 +11,10 @@ export const getUserInfo = () => {
       );
       resolve(accessToken);
     } catch (error) {
-      reject(error.response.data.message[0].message);
+      if (!axios.isAxiosError(error)) return reject("");
+      const errorData = error.response?.data ?? [];
+      if (!isExceptionMessageInterface(errorData)) return reject("");
+      reject(errorData.message[0].message);
     }
   });
 };
