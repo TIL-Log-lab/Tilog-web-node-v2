9e6de486b943128481cd5ec37306d8a64a8e1589
MINJE-98 <jmj012100@gmail.com>
2022-04-19 17:42:36 +0900

    feat: 나머지 버튼 추가

--- src/components/Organisms/Editor.tsx
+++ src/components/Organisms/Editor.tsx
@@ -4,7 +4,7 @@ import MPostEditor from "src/components/Molecules/PostEditor";
 
 const OEditor = () => {
   return (
-    <div className="w-[60rem] h-full">
+    <div className="my-5 w-[60rem] h-full">
       <MCoverImage />
       <input
         className="w-full px-10 text-4xl font-bold pt-7"
@@ -18,9 +18,17 @@ const OEditor = () => {
         <OCategorySearch />
       </div>
       <MPostEditor />
-      <button className="w-56 h-12 rounded bg-signature-color ">
-        <p className="text-lg text-white">등록</p>
-      </button>
+      <div className="flex w-1/2 space-x-5">
+        <button className="h-12 mt-3 text-lg text-white rounded w-28 bg-signature-color ">
+          등록
+        </button>
+        <button className="w-32 h-12 mt-3 text-lg rounded text-neutral-400 hover:text-white hover:bg-signature-color-blur">
+          비밀글로 등록
+        </button>
+        <button className="h-12 mt-3 rounded w-28 text-neutral-400 hover:bg-neutral-300 hover:text-white">
+          임시저장
+        </button>
+      </div>
     </div>
   );
 };
