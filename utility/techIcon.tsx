c26ce75c43eb9d303a00f1ca86cfcc4d9896c917
MINJE-98 <jmj012100@gmail.com>
2022-04-19 16:57:40 +0900

    feat: static techicon

---
+++ utility/techIcon.tsx
@@ -0,0 +1,64 @@
+import { ReactElement } from "react";
+import * as SI from "react-icons/si";
+import * as FA from "react-icons/fa";
+import * as GR from "react-icons/gr";
+import * as BS from "react-icons/bs";
+import * as CONST from "@Constants//techName";
+
+interface TechIcons {
+  [techName: string]: {
+    Render: () => ReactElement;
+  };
+}
+const techIcons: TechIcons = {
+  [CONST.JAVA_SCRIPT]: {
+    Render: () => <SI.SiJavascript className="text-javascript" />,
+  },
+  [CONST.TYPE_SCRIPT]: {
+    Render: () => <SI.SiTypescript className="text-typescript" />,
+  },
+  [CONST.HTML]: {
+    Render: () => <SI.SiHtml5 className="text-html" />,
+  },
+  [CONST.GIT]: {
+    Render: () => <SI.SiGit className="text-git" />,
+  },
+  [CONST.NESTJS]: {
+    Render: () => <SI.SiNestjs className="text-nestjs" />,
+  },
+  [CONST.REACT]: {
+    Render: () => <SI.SiReact className="text-react" />,
+  },
+  [CONST.SWIFT]: {
+    Render: () => <SI.SiSwift className="text-swift" />,
+  },
+  [CONST.DATABASE]: {
+    Render: () => <FA.FaDatabase className="text-database" />,
+  },
+  [CONST.MYSQL]: {
+    Render: () => <GR.GrMysql className="text-mysql" />,
+  },
+  [CONST.AWS]: {
+    Render: () => <SI.SiAmazonaws className="text-aws" />,
+  },
+  [CONST.DOCKER]: {
+    Render: () => <SI.SiDocker className="text-docker" />,
+  },
+  [CONST.DOCKER_COMPOSE]: {
+    Render: () => <SI.SiDocker className="text-docker" />,
+  },
+  [CONST.REDIS]: {
+    Render: () => <SI.SiRedis className="text-redis " />,
+  },
+  [CONST.NODEJS]: {
+    Render: () => <FA.FaNodeJs className="text-nodejs" />,
+  },
+  [CONST.CSS]: {
+    Render: () => <SI.SiCss3 className="text-css" />,
+  },
+  [CONST.INFRA]: {
+    Render: () => <BS.BsFillHddNetworkFill />,
+  },
+};
+
+export default techIcons;
