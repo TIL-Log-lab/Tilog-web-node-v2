f8be9ec46bfec69c74ec698263a32d07191dea2b
MINJE-98 <jmj012100@gmail.com>
2022-04-19 16:58:36 +0900

    feat: category search add

--- src/components/Organisms/Editor.tsx
+++ src/components/Organisms/Editor.tsx
@@ -1,3 +1,4 @@
+import OCategorySearch from "@Organisms/CategorySearch";
 import MCoverImage from "src/components/Molecules/Button/CoverImage";
 import MPostEditor from "src/components/Molecules/PostEditor";
 
@@ -13,12 +14,10 @@ const OEditor = () => {
         className="w-full px-10 text-2xl font-bold pt-7"
         placeholder="부제목"
       ></input>
-      <input
-        className="w-full px-10 text-xl font-bold pt-7"
-        placeholder="테그"
-      />
+      <div className="pl-10 bg-white dark:bg-neutral-900">
+        <OCategorySearch />
+      </div>
       <MPostEditor />
-
       <button className="w-56 h-12 rounded bg-signature-color ">
         <p className="text-lg text-white">등록</p>
       </button>
