0d4566011986dd974f8fef0aa7e0f54d3ba2a81d
MINJE-98 <jmj012100@gmail.com>
2022-04-20 17:32:16 +0900

    fix: 로그인 버튼 스타일 수정

--- src/components/Molecules/Button/Login.tsx
+++ src/components/Molecules/Button/Login.tsx
@@ -7,7 +7,7 @@ const MButtonLogin = () => {
   return (
     <button
       onClick={handleLogin}
-      className={`text-white rounded bg-black w-60 h-12`}
+      className={`text-white rounded bg-black w-fit h-12`}
     >
       <div className="flex flex-row items-center justify-center p-3 ">
         Login With Github
