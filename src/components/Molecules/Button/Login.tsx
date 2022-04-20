008e98c581396d5d0b6879bb6283f6a344adc6c0
MINJE-98 <jmj012100@gmail.com>
2022-04-20 16:59:42 +0900

    fix: 로그인 버튼 아이콘 추가, 디자인 수정

--- src/components/Molecules/Button/Login.tsx
+++ src/components/Molecules/Button/Login.tsx
@@ -1,10 +1,20 @@
+import MTechRender from "@Molecules/Icon/TechRender";
+
 const MButtonLogin = () => {
   const handleLogin = async () => {
     window.open("http://localhost/auth/github/login");
   };
   return (
-    <button onClick={handleLogin} className={`rounded bg-black w-72 h-12`}>
-      <p className="text-sm text-white">Login With Github</p>
+    <button
+      onClick={handleLogin}
+      className={`text-white rounded bg-black w-60 h-12`}
+    >
+      <div className="flex flex-row items-center justify-center p-3 ">
+        Login With Github
+        <div className="ml-3 text-2xl">
+          <MTechRender categoryName="Github" />
+        </div>
+      </div>
     </button>
   );
 };
