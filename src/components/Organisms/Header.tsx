1a48fb86599c257df10f1c19de601b481819ba30
MINJE-98 <jmj012100@gmail.com>
2022-04-20 17:32:37 +0900

    feat: 모바일 대응 코드 추가

--- src/components/Organisms/Header.tsx
+++ src/components/Organisms/Header.tsx
@@ -1,25 +1,38 @@
 import MButtonLogin from "@Molecules/Button/Login";
+import MButtonMobileLogin from "@Molecules/Button/MobileLogin";
 import { BsDot } from "react-icons/bs";
+import { useMediaQuery } from "react-responsive";
 
 const OHeader = ({ navTitle }: { navTitle: string }) => {
+  const isMobile = useMediaQuery({
+    query: "(max-width:1000px)",
+  });
   return (
     <div>
       <div className="grid grid-cols-3 p-5 font-eng-sub-font-2">
-        <div className="flex flex-row items-center justify-start text-xl ">
-          <a href="#">Today</a>
-          <BsDot />
-          <a href="#">MyBlog</a>
-          <BsDot />
-          <a href="#">Search</a>
-          <BsDot />
-          <a href="/editor">Write</a>
+        <div className="flex flex-row items-center justify-start text-sm">
+          {isMobile ? (
+            "="
+          ) : (
+            <>
+              <a href="#">Today</a>
+              <BsDot />
+              <a href="#">MyBlog</a>
+              <BsDot />
+              <a href="#">Search</a>
+              <BsDot />
+              <a href="/editor">Write</a>
+            </>
+          )}
         </div>
         <div className="flex flex-col items-center">
           <div className="w-16 h-8 rounded-tl-full rounded-tr-full bg-neutral-800 " />
-          {navTitle}
+          <p className="text-sm text-neutral-800 font-eng-sub-font-2">
+            {navTitle}
+          </p>
         </div>
-        <div className="flex justify-end">
-          <MButtonLogin />
+        <div className="flex items-center justify-end">
+          {isMobile ? <MButtonMobileLogin /> : <MButtonLogin />}
         </div>
       </div>
     </div>
