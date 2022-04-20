26071a65e509e50d3a1a68523eb5e321a6978cfe
MINJE-98 <jmj012100@gmail.com>
2022-04-20 17:00:06 +0900

    feat: 헤더 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import OHeader from "@Organisms/Header";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="p-10">
+    <div className="md:mx-20 2xl:mx-60">
+      <OHeader navTitle="Today" />
       <h1>Index Page...</h1>
     </div>
   );
