778ce15b81fa2dd04b32b99e542e69ecfd0c7fc4
MINJE-98 <jmj012100@gmail.com>
2022-04-22 18:18:05 +0900

    feat: 서버사이드에서 accesstoken과 userinfo 가져오기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppContext, AppProps } from "next/app";
-import { config } from "@Api/core";
 import "../styles/globals.css";
+import { config } from "@Api/core";
+import { getUserInfo } from "@Api/getUserInfo";
+import { getAccessToken } from "@Api/getAccessToken";
 
 const TILogApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
@@ -12,11 +14,21 @@ TILogApp.getInitialProps = async (context: AppContext) => {
 
   const headers = ctx.req?.headers;
   config.baseOptions = { headers: headers };
-  console.log(Component);
+
   if (Component.getInitialProps) {
     pageProps = await Component.getInitialProps(ctx);
   }
 
+  if (!headers) return { pageProps };
+  const cookie = headers.cookie;
+  if (!cookie || !cookie.includes("refreshToken")) return { pageProps };
+
+  const accessToken = await getAccessToken();
+  const userInfo = await getUserInfo(accessToken);
+  pageProps = {
+    accessToken: accessToken,
+    userInfo: userInfo,
+  };
   return { pageProps };
 };
 export default TILogApp;
