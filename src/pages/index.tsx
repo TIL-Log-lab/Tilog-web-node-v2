879c21f3b36d81bccab63ff1e9aceb51f12e75aa
MINJE-98 <jmj012100@gmail.com>
2022-04-23 18:55:15 +0900

    feat: 에러 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,25 +6,26 @@ import {
 } from "@til-log.lab/tilog-api";
 
 import OHeader from "@Organisms/Header";
+import { ExceptionMessageInterface } from "@Api/Errors/interface/messageError";
 interface HomeProps {
   accessToken: GetAccessTokenUsingRefreshTokenResponse;
-  userInfo: GetMeResponseDto | null;
+  userInfo: GetMeResponseDto;
+  error: ExceptionMessageInterface;
 }
 
-const Home: NextPage<HomeProps> = ({ accessToken, userInfo }: HomeProps) => {
+const Home: NextPage<HomeProps> = ({
+  accessToken,
+  userInfo,
+  error,
+}: HomeProps) => {
   return (
     <div className="md:mx-20 2xl:mx-60">
       <OHeader nav="Today" userInfo={userInfo} />
       <h1>Index Page...</h1>
-      <p>{accessToken}</p>
+      {error && <p>{error.message[0].message}</p>}
+      {accessToken && <p>{accessToken}</p>}
     </div>
   );
 };
-
-export async function getServerSideProps() {
-  return {
-    props: {}, // will be passed to the page component as props
-  };
-}
 
 export default Home;
