a5bb466dda00e265b74f411028420ab81ee1fed7
MINJE-98 <jmj012100@gmail.com>
2022-04-23 18:55:01 +0900

    fix: 로직 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { AxiosError } from "axios";
 import type { AppContext, AppProps } from "next/app";
 import "../styles/globals.css";
+
 import { config } from "@Api/core";
 import { getUserInfo } from "@Api/getUserInfo";
 import { getAccessToken } from "@Api/getAccessToken";
+import { isExceptionMessageInterface } from "@Api/Errors/interface/messageError";
+import { NoMessage } from "@Api/Errors/noMessage";
+import { NotResponse } from "@Api/Errors/notResponse";
 
 const TILogApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
@@ -20,28 +25,43 @@ TILogApp.getInitialProps = async (context: AppContext) => {
 
   if (!headers) {
     const pageProps = props;
-    return pageProps;
+    return { pageProps };
   }
   const cookie = headers.cookie;
 
   if (!cookie) {
     const pageProps = props;
-    return pageProps;
+    return { pageProps };
   }
   if (!cookie.includes("refreshToken")) {
     const pageProps = props;
-    return pageProps;
+    return { pageProps };
   }
 
-  const { accessToken } = await getAccessToken();
-  const userInfo = await getUserInfo(accessToken);
-
-  const pageProps = {
-    ...props,
-    accessToken,
-    userInfo,
-  };
-
-  return { pageProps };
+  try {
+    const getAccessTokenResponse = await getAccessToken();
+    const getUSerInfoResponse = await getUserInfo(
+      getAccessTokenResponse.accessToken
+    );
+    const pageProps = {
+      ...props,
+      accessToken: getAccessTokenResponse.accessToken,
+      userInfo: getUSerInfoResponse,
+    };
+    return { pageProps };
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.response) {
+      const errorData = axiosError.response.data ?? [];
+      const error = isExceptionMessageInterface(errorData)
+        ? errorData
+        : NoMessage;
+      const pageProps = { ...props, error: error };
+      return { pageProps };
+    } else {
+      const pageProps = { ...props, error: NotResponse };
+      return { pageProps };
+    }
+  }
 };
 export default TILogApp;
