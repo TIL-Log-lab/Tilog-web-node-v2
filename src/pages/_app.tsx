db650c1d78be2c6d06afc6c0e0e7cd424113dd20
MINJE-98 <jmj012100@gmail.com>
2022-04-25 13:45:22 +0900

    feat: react-query, accesstoken context, iron session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,67 +1,35 @@
-import { AxiosError } from "axios";
 import type { AppContext, AppProps } from "next/app";
 import "../styles/globals.css";
+import { getIronSession } from "iron-session";
+import { cookieConfig } from "@Constants/cookieConfig";
+import { AccessTokenProvider } from "src/context/AccessToken";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import { config } from "@Api/core";
-import { getUserInfo } from "@Api/getUserInfo";
-import { getAccessToken } from "@Api/getAccessToken";
-import { isExceptionMessageInterface } from "@Api/Errors/interface/messageError";
-import { NoMessage } from "@Api/Errors/noMessage";
-import { NotResponse } from "@Api/Errors/notResponse";
+const queryClient = new QueryClient({});
 
 const TILogApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AccessTokenProvider>
+        <Component {...pageProps} />
+      </AccessTokenProvider>
+    </QueryClientProvider>
+  );
 };
 
 TILogApp.getInitialProps = async (context: AppContext) => {
-  const { ctx, Component } = context;
+  const { ctx } = context;
 
-  const props = Component.getInitialProps
-    ? await Component.getInitialProps(ctx)
-    : {};
+  if (ctx.req && ctx.res) {
+    const session = await getIronSession(ctx.req, ctx.res, cookieConfig);
+    console.log(session);
 
-  const headers = ctx.req?.headers;
-  config.baseOptions = { headers: headers };
-
-  if (!headers) {
-    const pageProps = props;
-    return { pageProps };
-  }
-  const cookie = headers.cookie;
-
-  if (!cookie) {
-    const pageProps = props;
-    return { pageProps };
-  }
-  if (!cookie.includes("refreshToken")) {
-    const pageProps = props;
+    const pageProps = session;
     return { pageProps };
   }
 
-  try {
-    const getAccessTokenResponse = await getAccessToken();
-    const getUSerInfoResponse = await getUserInfo(
-      getAccessTokenResponse.accessToken
-    );
-    const pageProps = {
-      ...props,
-      accessToken: getAccessTokenResponse.accessToken,
-      userInfo: getUSerInfoResponse,
-    };
-    return { pageProps };
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    if (axiosError.response) {
-      const errorData = axiosError.response.data ?? [];
-      const errorResult = isExceptionMessageInterface(errorData)
-        ? errorData
-        : NoMessage;
-      const pageProps = { ...props, error: errorResult };
-      return { pageProps };
-    } else {
-      const pageProps = { ...props, error: NotResponse };
-      return { pageProps };
-    }
-  }
+  const pageProps = {};
+  return { pageProps };
 };
+
 export default TILogApp;
