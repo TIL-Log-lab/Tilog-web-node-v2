90c9d5d9c041df4d9d21446f2b158b30ea6b9baf
MINJE-98 <jmj012100@gmail.com>
2022-04-25 13:43:41 +0900

    feat: 헤더 컴포넌트 수정

--- src/components/Organisms/Header.tsx
+++ src/components/Organisms/Header.tsx
@@ -1,35 +1,24 @@
-import MButtonLogin from "@Molecules/Button/Login";
 import MButtonMobileLogin from "@Molecules/Button/MobileLogin";
 import { GetMeResponseDto } from "@til-log.lab/tilog-api";
 import { BsDot } from "react-icons/bs";
-import { useMediaQuery } from "react-responsive";
 
 interface OHeaderProps {
   nav: string | string[] | undefined;
-  userInfo: GetMeResponseDto | null;
+  userInfo: GetMeResponseDto;
 }
 
 const OHeader = ({ nav, userInfo }: OHeaderProps) => {
-  const isMobile = useMediaQuery({
-    query: "(max-width:1000px)",
-  });
   return (
     <div>
       <div className="grid grid-cols-3 p-5 font-eng-sub-font-2">
         <div className="flex flex-row items-center justify-start text-sm">
-          {isMobile ? (
-            "="
-          ) : (
-            <>
-              <a href="#">Today</a>
-              <BsDot />
-              <a href={`/blog/${userInfo}`}>MyBlog</a>
-              <BsDot />
-              <a href="#">Search</a>
-              <BsDot />
-              <a href="/editor">Write</a>
-            </>
-          )}
+          <a href="#">Today</a>
+          <BsDot />
+          {userInfo && <a href={`/blog/${userInfo.name}`}>MyBlog</a>}
+          <BsDot />
+          <a href="#">Search</a>
+          <BsDot />
+          <a href={"/editor"}>Write</a>
         </div>
         <div className="flex flex-col items-center">
           <div className="w-16 h-8 rounded-tl-full rounded-tr-full bg-neutral-800 " />
