b8b51c85b7538fb5aeb931b5f61396e33c6af6f3
MINJE-98 <jmj012100@gmail.com>
2022-04-27 14:38:34 +0900

    fix: 세션 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,8 +24,8 @@ TILogApp.getInitialProps = async (context: AppContext) => {
 
   if (!(ctx.req && ctx.res)) return { pageProps: {} };
 
-  const { userInfo } = await getIronSession(ctx.req, ctx.res, cookieConfig);
-  return { pageProps: userInfo };
+  const session = await getIronSession(ctx.req, ctx.res, cookieConfig);
+  return { pageProps: session };
 };
 
 export default TILogApp;
