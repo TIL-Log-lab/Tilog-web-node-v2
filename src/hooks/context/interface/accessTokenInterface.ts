4f9895bfad5f095e953eccd99d462d851ba64e57
MINJE-98 <jmj012100@gmail.com>
2022-04-27 12:35:22 +0900

    fix: accessToken interface 수정

--- src/hooks/context/interface/accessTokenInterface.ts
+++ src/hooks/context/interface/accessTokenInterface.ts
@@ -1,4 +1,8 @@
+import { GetAccessTokenUsingRefreshTokenResponse } from "@til-log.lab/tilog-api";
+import { AxiosError } from "axios";
+
 export interface AccessTokenInterface {
-  accessToken: string | null;
+  accessToken: GetAccessTokenUsingRefreshTokenResponse["accessToken"] | null;
   getAccessToken: () => void;
+  error: AxiosError | null;
 }
