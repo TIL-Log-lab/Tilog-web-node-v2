eb4ff58e3419780092858d5b02a42c52021b03d3
MINJE-98 <jmj012100@gmail.com>
2022-05-05 23:35:18 +0900

    fix: 에러 인터페이스 수정

--- lib/open-api/errors/interface/messageError.ts
+++ lib/open-api/errors/interface/messageError.ts
@@ -3,17 +3,16 @@ export const COUNTRY = {
   en: "en",
 } as const;
 
+export type ExceptionMessageInterface = Partial<
+  Record<typeof COUNTRY[keyof typeof COUNTRY], string>
+>;
+
+// NOTE:  오브젝트 키 검사
 export const isExceptionMessageInterface = (
   object: any
 ): object is ExceptionMessageInterface => {
-  const hadMessage = object ? "message" in object : false;
-  const hadCountryFlag = object?.message[0]?.countryFlag;
-  return !!(hadMessage && hadCountryFlag);
+  if (!object) return false;
+  if (!(object instanceof Object)) return false;
+
+  return Object.keys(object).some((key) => key in COUNTRY);
 };
-export interface ExceptionMessageInterface {
-  message: ExceptionMessageItemInterface[];
-}
-export interface ExceptionMessageItemInterface {
-  countryFlag: typeof COUNTRY[keyof typeof COUNTRY];
-  message: string;
-}
