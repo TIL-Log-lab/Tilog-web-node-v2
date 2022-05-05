621936ceff4447a35a67bdcf24b99bfdd75cc889
MINJE-98 <jmj012100@gmail.com>
2022-05-05 17:40:18 +0900

    fix: 인터페이스명 변경

--- lib/iron-session/interface/IronSessionData.ts
+++ lib/iron-session/interface/IronSessionData.ts
@@ -1,7 +1,7 @@
 import { GetMeResponseDto } from "@til-log.lab/tilog-api";
 
 declare module "iron-session" {
-  interface IronSessionUser {
+  interface IronSessionData {
     user?: GetMeResponseDto;
   }
 }
