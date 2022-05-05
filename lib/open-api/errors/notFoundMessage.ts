d8b0bcb366aeaa07b68b126e34bb1b7c1fc4014b
MINJE-98 <jmj012100@gmail.com>
2022-05-05 23:35:07 +0900

    fix: 파일 이름 변경

---
+++ lib/open-api/errors/notFoundMessage.ts
@@ -0,0 +1,6 @@
+import { ExceptionMessageInterface } from "@Api/errors/interface/messageError";
+
+export const notFoundMessage: ExceptionMessageInterface = {
+  ko: "에러 메세지가 존재하지 않습니다.",
+  en: "Error message does not exist.",
+};
