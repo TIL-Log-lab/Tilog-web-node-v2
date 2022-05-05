545d76a8aec7bde16e8e80534c3825e9558a0d03
MINJE-98 <jmj012100@gmail.com>
2022-05-05 17:41:14 +0900

    fix: refreshtoken, usersession 삭제

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,12 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { withIronSessionApiRoute } from "iron-session/next";
 import { cookieConfig } from "@Iron/cookieConfig";
+import axios from "axios";
 
 export default withIronSessionApiRoute(async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  req.session.destroy();
-  res.status(200).json({ ok: true });
+  const userAgent = !req.headers["user-agent"] ? "" : req.headers["user-agent"];
+  const cookie = !req.headers.cookie ? "" : req.headers.cookie;
+  try {
+    axios.delete("http://localhost/auth/logout", {
+      headers: {
+        "User-Agent": userAgent,
+        Cookie: cookie,
+      },
+    });
+    req.session.destroy();
+    return res.status(200).json({ ok: true });
+  } catch (error) {
+    if (!axios.isAxiosError(error)) {
+      return res.status(500).json({ ok: false, error: error });
+    }
+  }
 },
 cookieConfig);
