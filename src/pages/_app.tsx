e350e82fbb62202570fd43aafa35bb6cb146b60a
MINJE-98 <jmj012100@gmail.com>
2022-05-07 01:21:34 +0900

    feat: add user-info context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,22 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-import { AccessTokenProvider } from "@Hooks/context/AccessToken";
 import OHeader from "@Organisms/Header";
+import UserInfoProvider from "@Hooks/context/user-info/UserInfo";
+import { AccessTokenProvider } from "@Hooks/context/access-token/AccessToken";
 
 const queryClient = new QueryClient({});
 
 const TILogApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AccessTokenProvider>
-        <OHeader>
-          <Component {...pageProps} />
-        </OHeader>
-      </AccessTokenProvider>
+      <UserInfoProvider>
+        <AccessTokenProvider>
+          <OHeader>
+            <Component {...pageProps} />
+          </OHeader>
+        </AccessTokenProvider>
+      </UserInfoProvider>
     </QueryClientProvider>
   );
 };
