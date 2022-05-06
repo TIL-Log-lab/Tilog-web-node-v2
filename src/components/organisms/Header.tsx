6810c38e0fb45d65faa00d748184cd1d173d5c70
MINJE-98 <jmj012100@gmail.com>
2022-05-06 22:42:18 +0900

    fix: 세션에서 로컬스토리지로 변경

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,15 +1,17 @@
-import MButtonLogin from "src/components/molecules/button/Login";
-import { GetMeResponseDto } from "@til-log.lab/tilog-api";
+import { ReactNode } from "react";
 import { BsDot } from "react-icons/bs";
 import Link from "next/link";
+
 import MButtonLogout from "@Molecules/button/Logout";
+import useHeader from "@Hooks/custom/useHeader";
+import MButtonLogin from "@Molecules/button/Login";
 
 interface OHeaderProps {
-  nav: string | string[] | undefined;
-  userInfo: GetMeResponseDto | null;
+  children: ReactNode;
 }
 
-const OHeader = ({ nav, userInfo }: OHeaderProps) => {
+const OHeader = ({ children }: OHeaderProps) => {
+  const { userInfo, handleLogin, handleLogout } = useHeader();
   return (
     <div>
       <div className="grid grid-cols-3 p-5 font-eng-sub-font-2">
@@ -24,12 +26,17 @@ const OHeader = ({ nav, userInfo }: OHeaderProps) => {
         </div>
         <div className="flex flex-col items-center">
           <div className="w-16 h-8 rounded-tl-full rounded-tr-full bg-neutral-800 " />
-          <p className="text-sm text-neutral-800 font-eng-sub-font-2">{nav}</p>
+          <p className="text-sm text-neutral-800 font-eng-sub-font-2"></p>
         </div>
         <div className="flex items-center justify-end">
-          {!userInfo ? <MButtonLogin /> : <MButtonLogout />}
+          {!userInfo ? (
+            <MButtonLogin handleLogin={handleLogin} />
+          ) : (
+            <MButtonLogout handleLogout={handleLogout} />
+          )}
         </div>
       </div>
+      {children}
     </div>
   );
 };
