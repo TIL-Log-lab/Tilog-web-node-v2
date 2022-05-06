427149c17998d983c68b35ea7f7433892c1a4a5b
MINJE-98 <jmj012100@gmail.com>
2022-05-06 22:41:08 +0900

    feat: 유저 정보

---
+++ lib/open-api/setUserInfoToLocal.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { tilogApi } from "@Api/core";
+import { isExceptionMessageInterface } from "@Api/errors/interface/messageError";
+import { disconnectedServer } from "@Api/errors/disconnectedServer";
+import { notFoundMessage } from "@Api/errors/notFoundMessage";
+
+export default async function setUserInfoToLocal() {
+  try {
+    const {
+      data: { accessToken },
+    } = await tilogApi.usersAuthControllerGetAccessTokenUsingRefreshToken();
+
+    const { data } = await tilogApi.usersControllerGetMe({
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    window.localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    if (!axios.isAxiosError(error)) {
+      return error;
+    }
+    if (!error.response) return disconnectedServer;
+    const resData = error.response.data;
+    isExceptionMessageInterface(resData.message)
+      ? resData.message.ko
+      : notFoundMessage;
+  }
+}
