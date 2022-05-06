17d5e3ae06d3ade0aebe8dd15d32215a9554f88f
MINJE-98 <jmj012100@gmail.com>
2022-05-06 22:30:59 +0900

    fix: 서버사이드 렌더링 삭제

--- src/pages/blog/[username].tsx
+++ src/pages/blog/[username].tsx
@@ -1,32 +1,14 @@
-import { GetServerSideProps, NextPage } from "next";
-import { useRouter } from "next/router";
+import { NextPage } from "next";
 
-import OHeader from "@Organisms/Header";
 import OMyBlog from "@Organisms/MyBlog";
 
-import { GetMeResponseDto } from "@til-log.lab/tilog-api";
-import { getIronSession } from "iron-session";
-import { cookieConfig } from "@Iron/cookieConfig";
-interface BlogPageProps {
-  userInfo: GetMeResponseDto | null;
-}
-
-const BlogPage: NextPage<BlogPageProps> = ({ userInfo }: BlogPageProps) => {
-  const router = useRouter();
-  const { username } = router.query;
+const BlogPage: NextPage = () => {
   return (
     <div className="md:mx-20 2xl:mx-60">
-      <OHeader nav={username} userInfo={userInfo} />
       <div className="mt-20">
         <OMyBlog />
       </div>
     </div>
   );
-};
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { userInfo } = await getIronSession(ctx.req, ctx.res, cookieConfig);
-  return {
-    props: { userInfo: !userInfo ? null : userInfo },
-  };
 };
 export default BlogPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,15 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 
 import useHomeQuery from "@Hooks/react-query/useHomeQuery";
-import OHeader from "@Organisms/Header";
 
-import { GetMeResponseDto } from "@til-log.lab/tilog-api";
-import { getIronSession } from "iron-session";
-import { cookieConfig } from "@Iron/cookieConfig";
-interface HomeProps {
-  userInfo: GetMeResponseDto | null;
-}
-
-const Home: NextPage<HomeProps> = ({ userInfo }: HomeProps) => {
+const Home: NextPage = () => {
   const { data } = useHomeQuery();
+
   return (
     <div className="md:mx-20 2xl:mx-60">
-      <OHeader nav="Today" userInfo={userInfo} />
       <h1>Index Page...</h1>
       {data && <h2>{data.data.name}</h2>}
     </div>
   );
-};
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { userInfo } = await getIronSession(ctx.req, ctx.res, cookieConfig);
-  return {
-    props: { userInfo: !userInfo ? null : userInfo },
-  };
 };
 export default Home;
