496860e4f3edbe0de5dc74d45d225083aff43423
MINJE-98 <jmj012100@gmail.com>
2022-05-06 22:41:27 +0900

    fix: 데이터 관련 리팩터링

--- src/components/molecules/button/Login.tsx
+++ src/components/molecules/button/Login.tsx
@@ -1,39 +1,19 @@
-import axios from "axios";
-
 import OTechIcons from "src/components/organisms/techIcons";
+interface MButtonLoginProps {
+  handleLogin: () => void;
+}
 
-const MButtonLogin = () => {
-  const handleLogin = () => {
-    const loginWindow = window.open("http://localhost/auth/github/login");
-    if (!loginWindow) {
-      return alert("window open error");
-    }
-    const loginCheck = setInterval(async () => {
-      if (loginWindow.closed) {
-        clearInterval(loginCheck);
-      }
-      try {
-        await axios.get("http://localhost:3000/api/userinfo");
-        window.location.reload();
-        loginWindow.close();
-      } catch (error) {
-        if (axios.isAxiosError(error))
-          if (!error.response) alert(error.message);
-      }
-    }, 1000);
-  };
-  return (
-    <button
-      onClick={handleLogin}
-      className={`text-white rounded bg-black w-fit h-12`}
-    >
-      <div className="flex flex-row items-center justify-center p-3 ">
-        Login With Github
-        <div className="ml-3 text-2xl">
-          <OTechIcons categoryName="Github" />
-        </div>
+const MButtonLogin = ({ handleLogin }: MButtonLoginProps) => (
+  <button
+    onClick={handleLogin}
+    className={`text-white rounded bg-black w-fit h-12`}
+  >
+    <div className="flex flex-row items-center justify-center p-3 ">
+      Login With Github
+      <div className="ml-3 text-2xl">
+        <OTechIcons categoryName="Github" />
       </div>
-    </button>
-  );
-};
+    </div>
+  </button>
+);
 export default MButtonLogin;
