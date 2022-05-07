8d850c2b918b103892a129b7a2be33b26a752ad5
MINJE-98 <jmj012100@gmail.com>
2022-05-08 00:07:44 +0900

    feat: axios interceptor로 global handling

--- lib/open-api/core.ts
+++ lib/open-api/core.ts
@@ -1,9 +1,38 @@
+import errorException from "@Api/errors/exception";
+import { isExceptionMessageInterface } from "@Api/errors/interface/messageError";
+import { NETWORK_ERROR_MESSAGE } from "@Api/errors/message/networkErrorMessage";
+import { NOT_FOUND_MESSAGE } from "@Api/errors/message/notFoundMessage";
 import * as TILog from "@til-log.lab/tilog-api";
+import axios from "axios";
 
-export const config = new TILog.Configuration({
+const config = new TILog.Configuration({
   basePath: process.env.TILOG_API,
   baseOptions: {
     withCredentials: true,
   },
 });
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        const resData = error.response.data;
+        const isExceptionMessage = isExceptionMessageInterface(resData.message);
+        isExceptionMessage
+          ? Promise.reject(
+              errorException(error.response.status, resData.message)
+            )
+          : Promise.reject(
+              errorException(error.response.status, NOT_FOUND_MESSAGE)
+            );
+      } else if (error.request) {
+        return Promise.reject(errorException(500, NETWORK_ERROR_MESSAGE));
+      } else {
+        return Promise.reject(errorException(500, NETWORK_ERROR_MESSAGE));
+      }
+    }
+  }
+);
+
 export const tilogApi = new TILog.DefaultApi(config);
