d46158f47e920cbb723b588dafd8b1f0229b0836
MINJE-98 <jmj012100@gmail.com>
2022-05-07 17:26:52 +0900

    fix: message 폴더로 이동

--- lib/open-api/errors/message/disconnectedMessage.ts
+++ lib/open-api/errors/message/disconnectedMessage.ts
@@ -1,6 +1,6 @@
 import { ExceptionMessageInterface } from "@Api/errors/interface/messageError";
 
-export const disconnectedServer: ExceptionMessageInterface = {
+export const disconnectedMessage: ExceptionMessageInterface = {
   ko: "서버와 연결이 끊겼습니다.",
   en: "Disconnected from server.",
 };
