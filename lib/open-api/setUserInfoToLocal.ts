2e04e782d3259edf23a09b7d5fd8ca99deec491e
MINJE-98 <jmj012100@gmail.com>
2022-05-07 17:35:45 +0900

    fix: 리턴 타입 지정 및 message 폴더 이동

--- lib/open-api/setUserInfoToLocal.ts
+++ lib/open-api/setUserInfoToLocal.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
-import { tilogApi } from "@Api/core";
-import { isExceptionMessageInterface } from "@Api/errors/interface/messageError";
-import { disconnectedServer } from "@Api/errors/disconnectedServer";
-import { notFoundMessage } from "@Api/errors/notFoundMessage";
 
-export default async function setUserInfoToLocal() {
+import { tilogApi } from "@Api/core";
+import { disconnectedMessage } from "@Api/errors/message/disconnectedMessage";
+import { notFoundMessage } from "@Api/errors/message/notFoundMessage";
+
+import { isExceptionMessageInterface } from "@Api/errors/interface/messageError";
+import { GetMeResponseDto } from "@til-log.lab/tilog-api";
+
+export default async function setUserInfoToLocal(): Promise<GetMeResponseDto> {
   try {
     const {
       data: { accessToken },
@@ -14,13 +17,15 @@ export default async function setUserInfoToLocal() {
       headers: { Authorization: `Bearer ${accessToken}` },
     });
     window.localStorage.setItem("userInfo", JSON.stringify(data));
+    return data;
   } catch (error) {
-    if (!axios.isAxiosError(error)) {
-      return error;
+    if (axios.isAxiosError(error)) {
+      if (!error.response) throw disconnectedMessage;
+      const resData = error.response.data;
+      const isExceptionMessage = isExceptionMessageInterface(resData.message);
+      throw isExceptionMessage ? resData.message.ko : notFoundMessage;
+    } else {
+      throw notFoundMessage;
     }
-    if (!error.response) return disconnectedServer;
-    const resData = error.response.data;
-    const isExceptionMessage = isExceptionMessageInterface(resData.message);
-    return isExceptionMessage ? resData.message.ko : notFoundMessage;
   }
 }
