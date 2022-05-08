57d84b78d14f6ee44d6211bf57c4178fd8778a54
MINJE-98 <jmj012100@gmail.com>
2022-05-08 16:51:54 +0900

    feat: response interceptor error, language support

--- lib/open-api/core.ts
+++ lib/open-api/core.ts
@@ -1,9 +1,10 @@
-import errorException from "@Api/errors/exception";
-import { isExceptionMessageInterface } from "@Api/errors/interface/messageError";
-import { NETWORK_ERROR_MESSAGE } from "@Api/errors/message/networkErrorMessage";
-import { NOT_FOUND_MESSAGE } from "@Api/errors/message/notFoundMessage";
 import * as TILog from "@til-log.lab/tilog-api";
 import axios from "axios";
+
+import { exception, exceptionResponseMessageFilter } from "@Api/errors";
+import { ExceptionResponseBodyInterface } from "@Api/errors/interface/exceptionResponse";
+
+import { NETWORK_ERROR_MESSAGE } from "@Api/errors/constant/message/networkErrorMessage";
 
 const config = new TILog.Configuration({
   basePath: process.env.TILOG_API,
@@ -16,20 +17,25 @@ axios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (axios.isAxiosError(error)) {
+      const lang = window.navigator.language.includes("ko") ? "ko" : "en";
+      //     //NOTE: 서버에서 응답한 상태
       if (error.response) {
-        const resData = error.response.data;
-        const isExceptionMessage = isExceptionMessageInterface(resData.message);
-        isExceptionMessage
-          ? Promise.reject(
-              errorException(error.response.status, resData.message)
-            )
-          : Promise.reject(
-              errorException(error.response.status, NOT_FOUND_MESSAGE)
-            );
-      } else if (error.request) {
-        return Promise.reject(errorException(500, NETWORK_ERROR_MESSAGE));
-      } else {
-        return Promise.reject(errorException(500, NETWORK_ERROR_MESSAGE));
+        const resData: ExceptionResponseBodyInterface = error.response.data;
+
+        return Promise.reject(
+          exception(
+            error.response.status,
+            exceptionResponseMessageFilter(resData.message)[lang]
+          )
+        );
+      }
+      // NOTE: 서버에서 응답하지 못한 상태
+      else if (error.request) {
+        return Promise.reject(exception(500, NETWORK_ERROR_MESSAGE[lang]));
+      }
+      // NOTE: 요청, 응답이 모두 이루지지 않은 상태
+      else {
+        return Promise.reject(exception(500, NETWORK_ERROR_MESSAGE[lang]));
       }
     }
   }
