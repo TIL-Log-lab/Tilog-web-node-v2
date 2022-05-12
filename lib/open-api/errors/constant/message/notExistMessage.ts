6faa86739d92c96bcd9fbef8445d79012f6baa1b
MINJE-98 <jmj012100@gmail.com>
2022-05-12 10:23:46 +0900

    fix: 메세지 수정 및 이름 변경

---
+++ lib/open-api/errors/constant/message/notExistMessage.ts
@@ -0,0 +1,6 @@
+import { ExceptionMessageInterface } from "@Api/errors/interface/exception";
+
+export const NOT_EXIST_MESSAGE: ExceptionMessageInterface = {
+  ko: "에러 메세지가 존재하지 않습니다.",
+  en: "Error message does not exist.",
+};
