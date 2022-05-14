771c14b8dd63b5bb3bbcd5267262cff942a90f0c
MINJE-98 <jmj012100@gmail.com>
2022-05-14 18:51:25 +0900

    fix: requestLocation 상수변수 추가

--- lib/open-api/core.ts
+++ lib/open-api/core.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-import { exception } from "@Api/errors";
+import { exception } from "@Api/errors/exception";
 import getUserLanguage from "@Language";
 import * as TILog from "@til-log.lab/tilog-api";
 
 import { NETWORK_ERROR_MESSAGE } from "@Api/errors/constant/message/networkErrorMessage";
+import { REQUEST_ERROR, UNKNOWN } from "@Api/errors/constant/requestLocation";
 
 const config = new TILog.Configuration({
   basePath: process.env.TILOG_API,
@@ -35,14 +36,14 @@ axios.interceptors.response.use(
       // NOTE: 서버에서 응답하지 못한 상태
       else if (error.request) {
         return Promise.reject(
-          exception(500, "REQUEST_ERROR", NETWORK_ERROR_MESSAGE, userCountry)
+          exception(500, REQUEST_ERROR, NETWORK_ERROR_MESSAGE, userCountry)
         );
       }
 
       // NOTE: 요청, 응답이 모두 이루지지 않은 상태
       else {
         return Promise.reject(
-          exception(520, "UNKNOWN", NETWORK_ERROR_MESSAGE, userCountry)
+          exception(520, UNKNOWN, NETWORK_ERROR_MESSAGE, userCountry)
         );
       }
     }
