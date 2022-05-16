4971863ffb51afe616f9f646f5a4a3286d6b0adf
MINJE-98 <jmj012100@gmail.com>
2022-05-16 20:49:14 +0900

    fix: 에디터 컴포넌트를 재사용하기위한 props 추가

--- src/components/molecules/PostEditor.tsx
+++ src/components/molecules/PostEditor.tsx
@@ -1,10 +1,17 @@
 import StarterKit from "@tiptap/starter-kit";
 import { EditorContent, useEditor } from "@tiptap/react";
 
-const MPostEditor = () => {
+interface MPostEditorProps {
+  editorMode: boolean;
+  preViewContent: string;
+}
+
+const MPostEditor = ({ editorMode, preViewContent }: MPostEditorProps) => {
   const editor = useEditor({
     extensions: [StarterKit],
-    content: "",
+    autofocus: true,
+    editable: editorMode,
+    content: preViewContent,
     editorProps: {
       attributes: {
         class: `w-full h-full px-10 py-5 prose prose-sm max-w-none dark:prose-invert focus:outline-none overflow-y-auto prose-img:ml-auto prose-img:mr-auto prose-img:border prose-img:border-gray-200 dark:text-white text-natural-700 dark:prose-img:border-gray-700`,
