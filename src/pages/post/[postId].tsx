8870c889fc24f1dd5c109709537f2f5babe6d75b
MINJE-98 <jmj012100@gmail.com>
2022-05-16 20:34:43 +0900

    feat: postDetail ServerSideFething 작성

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,4 +1,5 @@
-import { NextPage } from "next";
+import { tilogApi } from "@Api/core";
+import { GetServerSideProps, NextPage } from "next";
 
 import PostDetail from "src/components/organisms/PostDetail";
 
@@ -11,5 +12,48 @@ const PostDetailPage: NextPage = () => {
       {/* <OComment /> */}
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params, req } = context;
+  if (!params) return { notFound: true };
+
+  const postId = params.postId;
+  if (!postId) return { notFound: true };
+  if (Array.isArray(postId)) return { notFound: true };
+
+  const responseResult =
+    await tilogApi.usersAuthControllerGetAccessTokenUsingRefreshToken(
+      undefined,
+      {
+        headers: {
+          "User-Agent": !req.headers["user-agent"]
+            ? ""
+            : req.headers["user-agent"],
+          Cookie: !req.headers.cookie ? "" : req.headers.cookie,
+        },
+      }
+    );
+  try {
+    if (!responseResult) {
+      const { data } = await tilogApi.postsControllerGetPostDetail(postId);
+      return {
+        props: {
+          postDetail: data,
+        },
+      };
+    }
+
+    const { data } = await tilogApi.postsControllerGetPostDetail(postId, {
+      headers: {
+        Authorization: `Bearer ${responseResult.data.accessToken}`,
+      },
+    });
+    return {
+      props: { postDetail: data },
+    };
+  } catch {
+    return { notFound: true };
+  }
 };
 export default PostDetailPage;
