c746e690122e0290a099ec88a50b02615c9bb46b
MINJE-98 <jmj012100@gmail.com>
2022-05-22 11:32:01 +0900

    feat: access-token api

---
+++ src/pages/api/access-token.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { TilogApiForAuth } from "@Api/core";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { headers } = req;
+  const userAgent = !headers["user-agent"] ? "" : headers["user-agent"];
+  const cookie = !headers.cookie ? "" : headers.cookie;
+
+  try {
+    const { data } =
+      await TilogApiForAuth.usersAuthControllerGetAccessTokenUsingRefreshToken(
+        undefined,
+        {
+          headers: {
+            "User-Agent": userAgent,
+            Cookie: cookie,
+          },
+        }
+      );
+    res.status(200).json(data);
+  } catch (error) {
+    res.send(error);
+  }
+};
+export default handler;
