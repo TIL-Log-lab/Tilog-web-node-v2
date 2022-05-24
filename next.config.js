befd98646ce582fe31be2c347d4356ecf9d62993
MINJE-98 <jmj012100@gmail.com>
2022-05-24 21:43:11 +0900

    chore: 환경변수 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  TILOG_API: process.env.TILOG_API,
+  env: {
+    TILOG_API: process.env.TILOG_API,
+    TILOG_INFO_COOKIE: process.env.TILOG_INFO_COOKIE,
+  },
 };
 
 module.exports = nextConfig;
