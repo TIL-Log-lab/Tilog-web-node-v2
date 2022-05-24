e8824a63cd9fb4d526049312eafb062318180c7b
MINJE-98 <jmj012100@gmail.com>
2022-05-24 12:03:26 +0900

    feat: 로그인 click 이벤트 이동, redux 적용

--- src/components/molecules/button/Login.tsx
+++ src/components/molecules/button/Login.tsx
@@ -1,19 +1,50 @@
-import OTechIcons from "@Organisms/techIcons";
-interface MButtonLoginProps {
-  handleLogin: () => void;
-}
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
 
-const MButtonLogin = ({ handleLogin }: MButtonLoginProps) => (
-  <button
-    onClick={handleLogin}
-    className={`text-white rounded bg-black w-fit h-12`}
-  >
-    <div className="flex flex-row items-center justify-center p-3 ">
-      Login With Github
-      <div className="ml-3 text-2xl">
-        <OTechIcons categoryName="Github" />
+import { userInfoSlice } from "@Redux/userInfo";
+import OTechIcons from "@Organisms/techIcons";
+import { TilogApiForAuth, TilogApiForUser } from "@Api/core";
+
+const MButtonLogin = () => {
+  const dispatch = useDispatch();
+  const handleLogin = () => {
+    const loginWindow = window.open("http://localhost/auth/github/login");
+    if (!loginWindow) {
+      return toast.error("window open error");
+    }
+    // NOTE: 로그인 체크
+    // TODO: alert -> toast
+    const loginCheck = setInterval(async () => {
+      if (loginWindow.closed) {
+        clearInterval(loginCheck);
+      }
+      try {
+        const {
+          data: { accessToken },
+        } = await TilogApiForAuth.usersAuthControllerGetAccessTokenUsingRefreshToken();
+
+        const { data } = await TilogApiForUser.usersControllerGetMe({
+          headers: { Authorization: `Bearer ${accessToken}` },
+        });
+        if (data) {
+          dispatch(userInfoSlice.actions.change(data));
+          loginWindow.close();
+        }
+      } catch (e) {}
+    }, 1000);
+  };
+  return (
+    <button
+      onClick={handleLogin}
+      className={`text-white rounded bg-black w-fit h-12`}
+    >
+      <div className="flex flex-row items-center justify-center p-3 ">
+        Login With Github
+        <div className="ml-3 text-2xl">
+          <OTechIcons categoryName="Github" />
+        </div>
       </div>
-    </div>
-  </button>
-);
+    </button>
+  );
+};
 export default MButtonLogin;
