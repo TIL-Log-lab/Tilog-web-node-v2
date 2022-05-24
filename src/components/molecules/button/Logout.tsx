c3ffd2425749c5e6fa5ccc6d36b566677b357ddd
MINJE-98 <jmj012100@gmail.com>
2022-05-24 12:03:32 +0900

    feat: 로그 아웃 click 이벤트 이동, redux 적용

--- src/components/molecules/button/Logout.tsx
+++ src/components/molecules/button/Logout.tsx
@@ -1,7 +1,23 @@
-interface MButtonLogoutProps {
-  handleLogout: () => void;
-}
-const MButtonLogout = ({ handleLogout }: MButtonLogoutProps) => (
-  <button onClick={handleLogout}>로그아웃</button>
-);
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+import { userInfoSlice } from "@Redux/userInfo";
+
+import { ExceptionInterface } from "@Api/errors/interface/exception.interface";
+
+const MButtonLogout = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    try {
+      dispatch(userInfoSlice.actions.resetUserInfo());
+      await axios.delete("http://localhost/auth/logout");
+    } catch (e) {
+      const error = e as ExceptionInterface;
+      toast.error(error.message);
+    }
+  };
+  return <button onClick={handleLogout}>로그아웃</button>;
+};
 export default MButtonLogout;
