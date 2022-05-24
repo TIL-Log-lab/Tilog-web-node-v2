03e219fa11472ccbd93d8fad5db32456d0aa1457
MINJE-98 <jmj012100@gmail.com>
2022-05-24 12:13:26 +0900

    fix: 필요없는 코드 삭제

--- src/hooks/context/user-info/interface/userInfo.interface.ts
+++
@@ -1,7 +0,0 @@
-import { GetMeResponseDto } from "@til-log.lab/tilog-api";
-
-export interface UserInfoInterface {
-  userInfo: GetMeResponseDto | null;
-  handleLogin: () => void;
-  handleLogout: () => void;
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,15 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "react-query";
-
 import { wrapper } from "@Redux/store";
-import OHeader from "@Organisms/Header";
-import UserInfoProvider from "@Context/user-info/UserInfo";
-import { AccessTokenProvider } from "@Context/access-token/AccessToken";
 
 const queryClient = new QueryClient({});
 
 const TILogApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <UserInfoProvider>
-        <AccessTokenProvider>
-          <OHeader>
-            <Toaster />
-            <Component {...pageProps} />
-          </OHeader>
-        </AccessTokenProvider>
-      </UserInfoProvider>
+      <Toaster />
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 };
