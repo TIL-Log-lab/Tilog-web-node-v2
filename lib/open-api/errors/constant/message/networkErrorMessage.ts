131503c86eddae12181a14ae3c9aec1fefd574b3
MINJE-98 <jmj012100@gmail.com>
2022-05-25 16:31:32 +0900

    fix: error message를 message로 이동

--- lib/open-api/errors/constant/message/networkErrorMessage.ts
+++
@@ -1,6 +0,0 @@
-import { ExceptionMessageInterface } from "@Api/errors/interface/exception.interface";
-
-export const NETWORK_ERROR_MESSAGE: ExceptionMessageInterface = {
-  ko: "서버와 연결이 끊겼습니다.",
-  en: "NetWork Error.",
-};
