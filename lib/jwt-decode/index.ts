872af8633e615f1140a00eeb966fff35208530e7
MINJE-98 <jmj012100@gmail.com>
2022-06-02 18:51:25 +0900

    feat: accessToken 만료 반환

---
+++ lib/jwt-decode/index.ts
@@ -0,0 +1,14 @@
+import { GetAccessTokenUsingRefreshTokenResponse } from "@til-log.lab/tilog-api";
+
+function isTokenExpired(
+  token: GetAccessTokenUsingRefreshTokenResponse["accessToken"]
+): boolean {
+  const payloadBase64 = token.split(".")[1];
+  const decodedJson = Buffer.from(payloadBase64, "base64").toString();
+  const decoded = JSON.parse(decodedJson);
+  const exp = decoded.exp;
+  const expired = Date.now() >= exp * 1000;
+  return expired;
+}
+
+export default isTokenExpired;
