1cf838f4ae794d6ab0440d4a5d21d2d175825225
MINJE-98 <jmj012100@gmail.com>
2022-06-03 22:11:18 +0900

    feat: Server Side Fetching authentication wrapper

---
+++ lib/auth/authentication.ts
@@ -0,0 +1,63 @@
+import { AxiosRequestHeaders } from "axios";
+
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+
+import { wrapper } from "@Redux/store";
+import { accessTokenSlice } from "@Redux/slices";
+
+import isTokenExpired from "@Auth/isTokenExpired";
+import getAccessToken from "@Api/server-side/getAccessToken";
+
+import { Store } from "@Redux/interfaces/redux.interface";
+import { GetAccessTokenUsingRefreshTokenResponse } from "@til-log.lab/tilog-api";
+
+export type Callback = (
+  store: Store,
+
+  context: GetServerSidePropsContext,
+
+  accessToken: GetAccessTokenUsingRefreshTokenResponse["accessToken"]
+) => Promise<GetServerSidePropsResult<{ [key: string]: any }>>;
+
+interface serverSideAuthenticationProps {
+  callback: Callback;
+}
+
+const serverSideAuthentication = ({
+  callback,
+}: serverSideAuthenticationProps) =>
+  wrapper.getServerSideProps((store) => async (context) => {
+    const { headers } = context.req;
+
+    const { isLogin } = store.getState().TILog_Info;
+
+    const { accessToken } = store.getState().accessToken;
+
+    const clientHeaders: AxiosRequestHeaders = {
+      "User-Agent": !headers["user-agent"] ? "" : headers["user-agent"],
+
+      Cookie: !headers.cookie ? "" : headers.cookie,
+    };
+
+    if (!isLogin) return callback(store, context, "");
+
+    try {
+      if (!accessToken || isTokenExpired(accessToken)) {
+        const { data } = await getAccessToken(clientHeaders);
+
+        store.dispatch(
+          accessTokenSlice.actions.changeToken({
+            accessToken: data.accessToken,
+          })
+        );
+
+        return callback(store, context, data.accessToken);
+      }
+
+      return callback(store, context, accessToken);
+    } catch (error) {
+      return callback(store, context, "");
+    }
+  });
+
+export default serverSideAuthentication;
