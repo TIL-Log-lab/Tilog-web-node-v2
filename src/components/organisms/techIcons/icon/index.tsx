6c2cca1993987822a89e9eed0c328caf6a12739b
MINJE-98 <jmj012100@gmail.com>
2022-06-04 15:22:34 +0900

    fix: JSX 반환 방식 변경

--- src/components/organisms/techIcons/icon/index.tsx
+++ src/components/organisms/techIcons/icon/index.tsx
@@ -5,81 +5,26 @@ import * as FA from "react-icons/fa";
 import * as GR from "react-icons/gr";
 import * as BS from "react-icons/bs";
 
-const TYPE_SCRIPT = "TypeScript";
-const JAVA_SCRIPT = "JavaScript";
-const HTML = "HTML";
-const REACT = "React";
-const SWIFT = "Swift";
-const DATABASE = "Database";
-const GITHUB = "Github";
-const NESTJS = "Nestjs";
-const MYSQL = "MySQL";
-const AWS = "AWS";
-const DOCKER = "Docker";
-const DOCKER_COMPOSE = "Docker-compose";
-const REDIS = "Redis";
-const CSS = "CSS";
-const NODEJS = "Nodejs";
-const INFRA = "Infra";
-const UNKNOWN = "Unknown";
-
-export interface TechIconInterface {
-  [techName: string]: {
-    Render: () => ReactElement;
-  };
+interface TechIconsProviderInterface {
+  [techName: string]: ReactElement;
 }
-const TechIcons: TechIconInterface = {
-  [JAVA_SCRIPT]: {
-    Render: () => <SI.SiJavascript className="text-javascript" />,
-  },
-  [TYPE_SCRIPT]: {
-    Render: () => <SI.SiTypescript className="text-typescript" />,
-  },
-  [HTML]: {
-    Render: () => <SI.SiHtml5 className="text-html" />,
-  },
-  [GITHUB]: {
-    Render: () => <SI.SiGithub className="text-git" />,
-  },
-  [NESTJS]: {
-    Render: () => <SI.SiNestjs className="text-nestjs" />,
-  },
-  [REACT]: {
-    Render: () => <SI.SiReact className="text-react" />,
-  },
-  [SWIFT]: {
-    Render: () => <SI.SiSwift className="text-swift" />,
-  },
-  [DATABASE]: {
-    Render: () => <FA.FaDatabase className="text-database" />,
-  },
-  [MYSQL]: {
-    Render: () => <GR.GrMysql className="text-mysql" />,
-  },
-  [AWS]: {
-    Render: () => <SI.SiAmazonaws className="text-aws" />,
-  },
-  [DOCKER]: {
-    Render: () => <SI.SiDocker className="text-docker" />,
-  },
-  [DOCKER_COMPOSE]: {
-    Render: () => <SI.SiDocker className="text-docker" />,
-  },
-  [REDIS]: {
-    Render: () => <SI.SiRedis className="text-redis " />,
-  },
-  [NODEJS]: {
-    Render: () => <FA.FaNodeJs className="text-nodejs" />,
-  },
-  [CSS]: {
-    Render: () => <SI.SiCss3 className="text-css" />,
-  },
-  [INFRA]: {
-    Render: () => <BS.BsFillHddNetworkFill />,
-  },
-  [UNKNOWN]: {
-    Render: () => <BS.BsFillPatchQuestionFill />,
-  },
-};
 
-export default TechIcons;
+const TechIconsProvider: TechIconsProviderInterface = {
+  JAVASCRIPT: <SI.SiJavascript className="text-javascript" />,
+  TYPE_SCRIPT: <SI.SiTypescript className="text-typescript" />,
+  HTML: <SI.SiHtml5 className="text-html" />,
+  GITHUB: <SI.SiGithub className="" />,
+  NESTJS: <SI.SiNestjs className="text-nestjs" />,
+  REACT: <SI.SiReact className="text-react" />,
+  SWIFT: <SI.SiSwift className="text-swift" />,
+  DATABASE: <FA.FaDatabase className="text-database" />,
+  MYSQL: <GR.GrMysql className="text-mysql" />,
+  AWS: <SI.SiAmazonaws className="text-aws" />,
+  DOCKER: <SI.SiDocker className="text-docker" />,
+  DOCKER_COMPOSE: <SI.SiDocker className="text-docker" />,
+  REDIS: <SI.SiRedis className="text-redis " />,
+  NODEJS: <FA.FaNodeJs className="text-nodejs" />,
+  CSS: <SI.SiCss3 className="text-css" />,
+  INFRA: <BS.BsFillHddNetworkFill />,
+};
+export default TechIconsProvider;
