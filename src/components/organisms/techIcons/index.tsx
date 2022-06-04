89ad6964eca63605c0b35608f3a977d552962f03
MINJE-98 <jmj012100@gmail.com>
2022-06-04 15:23:04 +0900

    fix: provider 반환 방식 변경인한 수정

--- src/components/organisms/techIcons/index.tsx
+++ src/components/organisms/techIcons/index.tsx
@@ -1,16 +1,16 @@
-import TechIcons from "src/components/organisms/techIcons/icon";
+import * as BS from "react-icons/bs";
+
+import techIconsProvider from "src/components/organisms/techIcons/icon";
 
 interface Props {
   categoryName: string;
 }
-const OTechIcons = ({ categoryName }: Props) => {
-  const { Render } = TechIcons[categoryName];
+const TechIcons = ({ categoryName }: Props) => {
+  const upper_categoryName = categoryName.toUpperCase();
 
-  if (!TechIcons[categoryName]) {
-    const { Render } = TechIcons["Unknown"];
-    return <Render />;
+  if (upper_categoryName in techIconsProvider) {
+    return techIconsProvider[upper_categoryName];
   }
-  return <Render />;
+  return <BS.BsFillPatchQuestionFill />;
 };
-
-export default OTechIcons;
+export default TechIcons;
