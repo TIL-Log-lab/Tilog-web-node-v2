1ec9c116ec85ef9563f6c86208b89809b838992b
MINJE-98 <jmj012100@gmail.com>
2022-06-06 17:41:19 +0900

    fix: token을 axios header에 저장

--- lib/auth/authentication.ts
+++ lib/auth/authentication.ts
@@ -3,18 +3,16 @@ import { AxiosRequestHeaders } from "axios";
 import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 
 import { wrapper } from "@Redux/store";
-import { accessTokenSlice } from "@Redux/slices";
 
 import isTokenExpired from "@Auth/isTokenExpired";
 import getAccessToken from "@Api/server-side/getAccessToken";
 
 import { Store } from "@Redux/interfaces/redux.interface";
-import { GetAccessTokenUsingRefreshTokenResponse } from "@til-log.lab/tilog-api";
+import { axiosInstance } from "@Api/core";
 
 export type Callback = (
   store: Store,
-  context: GetServerSidePropsContext,
-  accessToken: GetAccessTokenUsingRefreshTokenResponse["accessToken"] | null
+  context: GetServerSidePropsContext
 ) => Promise<GetServerSidePropsResult<{ [key: string]: any }>>;
 
 interface ServerSideAuthenticationProps {
@@ -26,10 +24,8 @@ const serverSideAuthentication = ({
 }: ServerSideAuthenticationProps) =>
   wrapper.getServerSideProps((store) => async (context) => {
     const { headers } = context.req;
-
     const { isLogin } = store.getState().TILog_Info;
-
-    const { accessToken } = store.getState().accessToken;
+    const accessToken = axiosInstance.defaults.headers.common["Authorization"];
 
     const clientHeaders: AxiosRequestHeaders = {
       "User-Agent": !headers["user-agent"] ? "" : headers["user-agent"],
@@ -37,24 +33,32 @@ const serverSideAuthentication = ({
       Cookie: !headers.cookie ? "" : headers.cookie,
     };
 
-    if (!isLogin) return callback(store, context, null);
+    if (!isLogin) return callback(store, context);
 
     try {
-      if (!accessToken || isTokenExpired(accessToken)) {
+      if (!accessToken) {
         const { data } = await getAccessToken(clientHeaders);
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `bearer ${data.accessToken}`;
 
-        store.dispatch(
-          accessTokenSlice.actions.changeToken({
-            accessToken: data.accessToken,
-          })
-        );
-
-        return callback(store, context, data.accessToken);
+        return callback(store, context);
       }
-
-      return callback(store, context, accessToken);
+      const accessTokenToString = axiosInstance.defaults.headers.common[
+        "Authorization"
+      ]
+        .toString()
+        .slice(7);
+      if (isTokenExpired(accessTokenToString)) {
+        const { data } = await getAccessToken(clientHeaders);
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `bearer ${data.accessToken}`;
+        return callback(store, context);
+      }
+      return callback(store, context);
     } catch (error) {
-      return callback(store, context, null);
+      return callback(store, context);
     }
   });
 
