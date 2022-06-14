ed0dbd1b9e80583ff34a2286d03585fe21bebd20
MINJE-98 <jmj012100@gmail.com>
2022-06-14 11:48:43 +0900

    fix: rename, promise remove

--- src/hooks/react-query/auth-wrapper/useAuthMutation.ts
+++ src/hooks/react-query/auth-wrapper/useAuthMutation.ts
@@ -2,11 +2,8 @@ import { useMutation } from "react-query";
 
 import clientSideAuthentication from "@Auth/clientSideAuthentication";
 
-//TODO: 메서드명 변경해야함.
-export const useWrappingToastAuthMutation = <T>(
-  callback: (data: T) => void
-) => {
-  return useMutation(
-    async (data: T) => await clientSideAuthentication(() => callback(data))
+export const useAuthMutation = <T>(callback: (data: T) => void) => {
+  return useMutation((data: T) =>
+    clientSideAuthentication(() => callback(data))
   );
 };
