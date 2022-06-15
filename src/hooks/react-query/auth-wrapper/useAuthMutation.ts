dad0ad54e2a9ffb7d8dbfd27ce27f2cb865c5e9b
MINJE-98 <jmj012100@gmail.com>
2022-06-16 02:17:43 +0900

    feat: authmutation type

--- src/hooks/react-query/auth-wrapper/useAuthMutation.ts
+++ src/hooks/react-query/auth-wrapper/useAuthMutation.ts
@@ -1,9 +1,10 @@
 import { useMutation } from "react-query";
 
 import clientSideAuthentication from "@Auth/clientSideAuthentication";
+import ExceptionInterface from "@Api/errors/interfaces";
 
 export const useAuthMutation = <T>(callback: (data: T) => void) => {
-  return useMutation((data: T) =>
+  return useMutation<void, ExceptionInterface, T>((data: T) =>
     clientSideAuthentication(() => callback(data))
   );
 };
