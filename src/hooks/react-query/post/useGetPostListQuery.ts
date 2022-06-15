a00c331a0b4391eb5b7f90960c246eb9d3237d81
MINJE-98 <jmj012100@gmail.com>
2022-06-15 21:34:28 +0900

    feat: post InfiniteQuery

---
+++ src/hooks/react-query/post/useGetPostListQuery.ts
@@ -0,0 +1,57 @@
+import { AxiosResponse } from "axios";
+import { useInfiniteQuery } from "react-query";
+
+import { getPosts } from "@Api/adapter";
+
+import ExceptionInterface from "@Api/errors/interfaces";
+import { GetPostsRequestDto } from "@Api/interface/post";
+import { GetPostsResponseDto } from "@til-log.lab/tilog-api";
+
+interface GetPostListQueryInterface extends GetPostsRequestDto {
+  queryKey: string;
+}
+
+const useGetPostListQuery = ({
+  queryKey,
+  dateScope,
+  sortScope,
+  maxContent,
+  userId,
+  page,
+  categoryId,
+}: GetPostListQueryInterface) => {
+  return useInfiniteQuery<
+    AxiosResponse<GetPostsResponseDto>,
+    ExceptionInterface,
+    AxiosResponse<GetPostsResponseDto>,
+    string
+  >(
+    queryKey,
+    async ({ pageParam = page }) => {
+      const result = await getPosts(
+        dateScope,
+        sortScope,
+        pageParam,
+        maxContent,
+        userId,
+        categoryId
+      );
+      return result;
+    },
+    {
+      retry: 0,
+      refetchOnWindowFocus: false,
+
+      getNextPageParam: (lastPages, pages) => {
+        const nextPage = pages.length;
+        const lastPagesListCount = lastPages.data.list.length;
+
+        if (lastPagesListCount != 0 && lastPagesListCount % maxContent === 0) {
+          return nextPage;
+        }
+      },
+    }
+  );
+};
+
+export default useGetPostListQuery;
