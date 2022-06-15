b76934a53528f4431cb4be5d3c46c9e4d7eadf87
MINJE-98 <jmj012100@gmail.com>
2022-06-16 00:22:44 +0900

    feat: like query

---
+++ src/hooks/react-query/like/useGetHasLikeQuery.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+
+import clientSideAuthentication from "@Auth/clientSideAuthentication";
+import { hasLiked } from "@Api/adapter/postLike";
+import { AxiosResponse } from "axios";
+import { HasLikedResponseDto } from "@til-log.lab/tilog-api";
+import ExceptionInterface from "@Api/errors/interfaces";
+
+export default function useGetHasLikeQuery(postId: string) {
+  return useQuery<
+    AxiosResponse<HasLikedResponseDto>,
+    ExceptionInterface,
+    AxiosResponse<HasLikedResponseDto>,
+    string
+  >(
+    "like",
+    async () => await clientSideAuthentication(() => hasLiked(postId)),
+    {
+      retry: 0,
+      refetchOnWindowFocus: false,
+    }
+  );
+}
