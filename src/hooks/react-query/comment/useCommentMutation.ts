1fcaf5a51417e7e5d1b979f6a0edbf5c5e43c129
MINJE-98 <jmj012100@gmail.com>
2022-06-16 11:11:07 +0900

    feat: query mutation 구현

---
+++ src/hooks/react-query/comment/useCommentMutation.ts
@@ -0,0 +1,32 @@
+import { createComment, deleteComment, updateComment } from "@Api/adapter";
+import { useAuthMutation } from "@Query/auth-wrapper/useAuthMutation";
+import {
+  CreateCommentsRequestBodyDto,
+  DeleteCommentRequestDto,
+  UpdateCommentRequestDto,
+} from "@til-log.lab/tilog-api";
+
+export const useDeleteCommentMutation = () => {
+  return useAuthMutation<DeleteCommentRequestDto>(
+    ({ commentId }: { commentId: string }) =>
+      deleteComment({
+        commentId: commentId,
+      })
+  );
+};
+
+export const useUpdateCommentMutation = () => {
+  return useAuthMutation<UpdateCommentRequestDto>(
+    ({ commentId, content }: { commentId: string; content: string }) =>
+      updateComment({
+        commentId: commentId,
+        content: content,
+      })
+  );
+};
+
+export const useCreateCommentMutation = () => {
+  return useAuthMutation<CreateCommentsRequestBodyDto>((data) =>
+    createComment(data)
+  );
+};
