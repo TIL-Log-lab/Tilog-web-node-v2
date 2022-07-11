2c21004c88d0891eaa3ad930606ce4e2c2942e0b
MINJE-98 <jmj012100@gmail.com>
2022-07-11 18:27:31 +0900

    fix: 코드 수정 절대경로 수정 axios 코드 리팩터링으로 인한 수정 modal에 필요없는 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,25 @@ import type { NextPage } from "next";
 
 import { wrapper } from "@Redux/store";
 import OHeader from "@Organisms/Header";
-import useHomeQuery from "@Query/useHomeQuery";
+
 import { modalSlice } from "@Redux/modal";
 import { useDispatch } from "react-redux";
+import api from "@Library/api";
 
 const Home: NextPage = () => {
-  const { data } = useHomeQuery();
   const dispatch = useDispatch();
   return (
     <div className="md:mx-20 2xl:mx-60">
       <OHeader />
       <h1>Index Page...</h1>
       <button
-        onClick={() =>
-          dispatch(modalSlice.actions.setModal({ modalName: "LOGIN" }))
-        }
+        type="button"
+        onClick={async () => {
+          console.log(await api.usersService.getMe());
+        }}
       >
-        로그인 모달
+        누르기
       </button>
-      {data && <h2>{data.data.name}</h2>}
     </div>
   );
 };
