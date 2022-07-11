e991bbf0b8e30b69741048ab9b44bf02cb9857bf
MINJE-98 <jmj012100@gmail.com>
2022-07-11 18:28:03 +0900

    fix: 이전코드 삭제

--- lib/auth/index.ts
+++
@@ -1,4 +0,0 @@
-import serverSideAuthentication from "@Auth/serverSideAuthentication";
-import clientSideAuthentication from "@Auth/clientSideAuthentication";
-
-export { serverSideAuthentication, clientSideAuthentication };
--- lib/open-api/errors/constant/requestLocation.ts
+++
@@ -1,2 +0,0 @@
-export const REQUEST_ERROR = "REQUEST_ERROR";
-export const UNKNOWN = "UNKNOWN";
