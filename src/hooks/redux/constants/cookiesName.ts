a2740534ccf53b74ab7a8f1cc0a67b2c10299f8c
MINJE-98 <jmj012100@gmail.com>
2022-07-12 21:12:12 +0900

    fix: 환경변수 이동 환경변수를 전역에서 관리합니다.

--- src/hooks/redux/constants/cookiesName.ts
+++
@@ -1,3 +0,0 @@
-export const TILOG_INFO_COOKIE = !process.env.TILOG_INFO_COOKIE
-  ? "TILog_Info"
-  : process.env.TILOG_INFO_COOKIE;
--- src/hooks/redux/constants/modalName.tsx
+++
@@ -1,6 +0,0 @@
-import OLogin from "@Organisms/Login";
-
-const MODAL_NAME = {
-  LOGIN: <OLogin />,
-} as const;
-export default MODAL_NAME;
