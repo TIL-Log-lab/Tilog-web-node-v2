cbcf4c8cc9560b96261f2735fe8f896dc041f786
MINJE-98 <jmj012100@gmail.com>
2022-07-14 17:35:32 +0900

    fix: 인터페이스 수정 반영

--- lib/api/exception/unknownError.ts
+++ lib/api/exception/unknownError.ts
@@ -1,13 +1,13 @@
 import { UNKNOWN_LOCATION } from "@Library/api/exception/constant/requestLocation";
 import { INTERNAL_SERVER_ERROR } from "@Library/api/exception/constant/statusCode";
 import ExceptionInterface from "@Library/api/exception/interface";
+import { LanguageInterface } from "@Library/language/interface";
 import { NETWORK_ERROR_MESSAGE } from "@Library/messages/constants/error";
-import MessageInterface from "@Library/messages/interface";
 
 export default class UnknownError implements ExceptionInterface {
   readonly statusCode: number;
   readonly requestLocation: string;
-  readonly message: MessageInterface;
+  readonly message: LanguageInterface;
 
   constructor() {
     this.statusCode = INTERNAL_SERVER_ERROR;
