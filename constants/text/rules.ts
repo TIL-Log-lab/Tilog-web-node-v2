2ca0fa2d5d27a350ded55afc109c553225dd90c8
MINJE-98 <jmj012100@gmail.com>
2022-07-15 19:12:48 +0900

    feat: 상수값

---
+++ constants/text/rules.ts
@@ -0,0 +1,20 @@
+import LANGUAGE from "@Library/language/constant";
+import {
+  LanguageInterface,
+  LanguageUnionType,
+} from "@Library/language/interface";
+
+export const MAX_LENGTH = (maxLength: number, language?: LanguageUnionType) => {
+  if (language === "ko") return `${maxLength}자 이상 입력이 제한되어 있습니다.`;
+  return `${maxLength}characters or more than input is limited.`;
+};
+
+export const MIN_LENGTH = (minLength: number, language?: LanguageUnionType) => {
+  if (language === "ko") return `${minLength}자 이하 입력이 제한되어 있습니다.`;
+  return `${minLength}characters or less input is limited.`;
+};
+
+export const WRONG_EMAIL: LanguageInterface = {
+  [LANGUAGE.ko]: `잘못된 이메일 형식입니다.`,
+  [LANGUAGE.en]: `Invalid email format.`,
+};
