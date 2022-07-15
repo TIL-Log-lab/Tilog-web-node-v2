1df99cb1ef1201440a9aeb22bf4c9fb87360f19a
MINJE-98 <jmj012100@gmail.com>
2022-07-15 19:19:30 +0900

    refactor: setting 유기체 수정 분리된 컴포넌트 적용 메세지 및 텍스트 상수화 및 언어 지정 새로운 api 호출방식 적용

--- src/components/organisms/user-settings/index.tsx
+++ src/components/organisms/user-settings/index.tsx
@@ -1,32 +1,47 @@
-import toast from "react-hot-toast";
-import { useForm, SubmitHandler } from "react-hook-form";
-
-import {
-  GetMeResponseDto,
-  SetSettingRequestBodyDtoSettingTypeEnum,
-} from "@til-log.lab/tilog-api";
-import { UserSettingTypes } from "@Organisms/user-settings/interface";
-import ExceptionInterface from "@Api/errors/interfaces";
-import { useDispatch, useSelector } from "react-redux";
-import { userInfoSelector, userInfoSlice } from "@Redux/userInfo";
-import clientSideAuthentication from "@Auth/clientSideAuthentication";
-import { WRONG_EMAIL } from "@Messages/constants/error/form";
-import { modalSlice } from "@Redux/modal";
 import React from "react";
-import { getMe, setSetting } from "@Api/adapter";
-import SettingInput from "@Organisms/user-settings/SettingInput";
 
-const OUserSetting = () => {
-  const { settings } = useSelector(userInfoSelector);
+import { SubmitHandler, FormProvider } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+
+import SettingInput from "@Components/molecules/input/SettingInput";
+import useSettingForm from "@Components/organisms/user-settings/hooks/useSettingForm";
+import { UserSettingTypes } from "@Components/organisms/user-settings/interface";
+import {
+  displayNameRules,
+  emailRules,
+  introMsgRules,
+  positionRules,
+} from "@Components/organisms/user-settings/rules";
+import {
+  DISPLAY_NAME_DESC,
+  EMAIL_DESC,
+  INTRO_MSG_DESC,
+  POSITION_DESC,
+} from "@Constants/text/desc";
+import SUMMIT from "@Constants/text/summit";
+import {
+  DISPLAY_NAME_TITLE,
+  EMAIL_TITLE,
+  INTRO_MSG_TITLE,
+  POSITION_TITLE,
+} from "@Constants/text/title";
+import api from "@Library/api";
+import ExceptionInterface from "@Library/api/exception/interface";
+import SAVE_MESSAGE from "@Library/messages/constants/success/save";
+import {
+  languageSelector,
+  userInfoSlice,
+  userSettingSelector,
+} from "@Redux/userInfo";
+
+import { SetSettingRequestBodyDtoSettingTypeEnum } from "@til-log.lab/tilog-api";
+
+const UserSettings = () => {
+  const settings = useSelector(userSettingSelector);
+  const language = useSelector(languageSelector);
+  const method = useSettingForm(settings);
   const dispatch = useDispatch();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<UserSettingTypes>({
-    defaultValues: getUserSettings(settings),
-  });
-
   // NOTE: 설정 제출 이벤트
   const onSubmit: SubmitHandler<UserSettingTypes> = async (object) => {
     const loading = toast.loading("로딩중...");
@@ -35,139 +50,70 @@ const OUserSetting = () => {
       await Promise.all([
         ...Object.keys(object).map((key) => {
           const TKey = key as SetSettingRequestBodyDtoSettingTypeEnum;
-          return clientSideAuthentication(() =>
-            setSetting({
-              content: object[TKey],
-              settingType: TKey,
-            })
-          );
+          return api.usersService.setSetting(object[TKey], TKey);
         }),
       ]);
 
-      // NOTE: 새로운 유저 정보를 가져옴
-      const { data } = await clientSideAuthentication(() => getMe());
-
+      const { data } = await api.usersService.getMe();
       // NOTE: 유저 정보를 Redux와 동기화
       if (data) {
-        dispatch(userInfoSlice.actions.changeUserSetting(data.settings));
-        toast.success("저장 성공!", {
+        dispatch(
+          userInfoSlice.actions.changeUserInfo({
+            ...data,
+            isLogin: true,
+            language,
+          })
+        );
+        toast.success(SAVE_MESSAGE[language], {
           id: loading,
         });
       }
     } catch (e) {
       const error = e as ExceptionInterface;
-      if (error.statusCode === 401) {
-        dispatch(modalSlice.actions.setModal({ modalName: "LOGIN" }));
-      }
 
-      toast.error(error.message.en, {
+      toast.error(error.message[language], {
         id: loading,
       });
     }
   };
+
   return (
-    <form
-      className="grid w-full max-w-2xl gap-4"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <SettingInput
-        register={register}
-        type={SetSettingRequestBodyDtoSettingTypeEnum["DisplayName"]}
-        rules={{
-          minLength: {
-            value: 2,
-            message: "최소2자로 제한되어 있습니다.",
-          },
-          maxLength: {
-            value: 190,
-            message: "최대190자로 제한되어 있습니다.",
-          },
-        }}
-        title={"닉네임"}
-        subTitle={"닉네임을 설정하세요."}
-        errors={errors}
-      />
-      <SettingInput
-        register={register}
-        type={SetSettingRequestBodyDtoSettingTypeEnum["Email"]}
-        rules={{
-          pattern: {
-            value:
-              /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i,
-            message: WRONG_EMAIL,
-          },
-          maxLength: {
-            value: 190,
-            message: "최대190자로 제한되어 있습니다.",
-          },
-        }}
-        title={"이메일"}
-        subTitle={"이메일을 설정하세요."}
-        errors={errors}
-      />
-      <SettingInput
-        register={register}
-        type={SetSettingRequestBodyDtoSettingTypeEnum["IntroMsg"]}
-        rules={{
-          minLength: {
-            value: 10,
-            message: "최소10자로 제한되어 있습니다.",
-          },
-          maxLength: {
-            value: 190,
-            message: "최대190자로 제한되어 있습니다.",
-          },
-        }}
-        title={"개인소개"}
-        subTitle={"개인소개를 설정하세요."}
-        errors={errors}
-      />
-      <SettingInput
-        register={register}
-        type={SetSettingRequestBodyDtoSettingTypeEnum["Position"]}
-        rules={{
-          minLength: {
-            value: 10,
-            message: "최소10자로 제한되어 있습니다.",
-          },
-          maxLength: {
-            value: 190,
-            message: "최대190자로 제한되어 있습니다.",
-          },
-        }}
-        title={"포지션"}
-        subTitle={"포지션을 설정하세요."}
-        errors={errors}
-      />
-      <input
-        type="submit"
-        value="수정"
-        className="w-20 h-10 text-white bg-black rounded-md cursor-pointer "
-      />
-    </form>
+    <FormProvider {...method}>
+      <form
+        className="grid w-full max-w-2xl gap-4"
+        onSubmit={method.handleSubmit(onSubmit)}
+      >
+        <SettingInput
+          title={DISPLAY_NAME_TITLE[language]}
+          desc={DISPLAY_NAME_DESC[language]}
+          settingType={SetSettingRequestBodyDtoSettingTypeEnum.DisplayName}
+          rules={displayNameRules(language)}
+        />
+        <SettingInput
+          title={EMAIL_TITLE[language]}
+          desc={EMAIL_DESC[language]}
+          settingType={SetSettingRequestBodyDtoSettingTypeEnum.Email}
+          rules={emailRules(language)}
+        />
+        <SettingInput
+          title={INTRO_MSG_TITLE[language]}
+          desc={INTRO_MSG_DESC[language]}
+          settingType={SetSettingRequestBodyDtoSettingTypeEnum.IntroMsg}
+          rules={introMsgRules(language)}
+        />
+        <SettingInput
+          title={POSITION_TITLE[language]}
+          desc={POSITION_DESC[language]}
+          settingType={SetSettingRequestBodyDtoSettingTypeEnum.Position}
+          rules={positionRules(language)}
+        />
+        <input
+          type="submit"
+          value={SUMMIT[language]}
+          className="w-20 h-10 text-white bg-black rounded-md cursor-pointer "
+        />
+      </form>
+    </FormProvider>
   );
 };
-export default OUserSetting;
-
-// TODO: Refactoring
-function getUserSettings(
-  settings: GetMeResponseDto["settings"] | undefined
-): UserSettingTypes {
-  if (settings) {
-    return settings.reduce(
-      (prevSettings, curSettings) => {
-        return {
-          ...prevSettings,
-          [curSettings.type]: curSettings.content,
-        };
-      },
-      { DISPLAY_NAME: null, INTRO_MSG: null, EMAIL: null, POSITION: null }
-    );
-  }
-  return {
-    DISPLAY_NAME: null,
-    INTRO_MSG: null,
-    EMAIL: null,
-    POSITION: null,
-  };
-}
+export default UserSettings;
