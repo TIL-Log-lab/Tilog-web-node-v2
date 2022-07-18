8a29ab68802447ed04cda48281530fbfd5fdfda3
MINJE-98 <jmj012100@gmail.com>
2022-07-18 22:12:21 +0900

    fix: 글씨 크기 수정

--- src/components/atom/text/SubTitle.tsx
+++ src/components/atom/text/SubTitle.tsx
@@ -2,7 +2,7 @@ import { SubTitleProps } from "@Components/atom/text/interface";
 
 const SubTitle = ({ SubTitleStyle, children }: SubTitleProps) => (
   <h3
-    className={`${SubTitleStyle} text-neutral-500 text-xl font-medium font-eng-sub-font-2`}
+    className={`${SubTitleStyle} text-neutral-500 dark:text-neutral-300 my-1 text-sm font-medium font-eng-sub-font-2`}
   >
     {children}
   </h3>
--- src/components/atom/text/Title.tsx
+++ src/components/atom/text/Title.tsx
@@ -2,7 +2,7 @@ import { TitleProps } from "@Components/atom/text/interface";
 
 const Title = ({ TitleStyle, children }: TitleProps) => (
   <h1
-    className={`${TitleStyle} text-neutral-800 text-5xl font-extrabold font-eng-sub-font-1`}
+    className={`${TitleStyle} text-neutral-800 dark:text-neutral-50 text-xl my-1 font-extrabold font-eng-sub-font-1`}
   >
     {children}
   </h1>
