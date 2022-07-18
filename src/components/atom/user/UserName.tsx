9b6a5ba137fcf806e69f4812995ce89106398588
MINJE-98 <jmj012100@gmail.com>
2022-07-18 21:54:55 +0900

    fix: 텍스트 크기, 마진값 수정

--- src/components/atom/user/UserName.tsx
+++ src/components/atom/user/UserName.tsx
@@ -2,7 +2,7 @@ import UserNameProp from "@Components/atom/user/interface/userNameProp";
 
 const UserName = ({ userNameStyle, children }: UserNameProp) => (
   <span
-    className={`ml-3 text-xl font-semibold text-neutral-600 dark:text-neutral-50 font-eng-sub-font-1 ${userNameStyle}`}
+    className={`${userNameStyle} ml-3 text-xs mt-1 font-semibold text-neutral-600 dark:text-neutral-50 font-eng-sub-font-1 `}
   >
     {children}
   </span>
