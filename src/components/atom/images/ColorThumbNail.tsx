9bd6c407fe52c98d64f5ac6bf1e75f66e5160ae4
MINJE-98 <jmj012100@gmail.com>
2022-07-18 21:46:40 +0900

    fix: 텍스트 오버플로우, 텍스트 크기

--- src/components/atom/images/ColorThumbNail.tsx
+++ src/components/atom/images/ColorThumbNail.tsx
@@ -9,7 +9,7 @@ const ColorThumbNail = ({ title, color }: ColorThumbNailProps) => {
       className="flex items-center justify-center w-full h-full overflow-hidden"
     >
       <span
-        className="px-10 text-2xl font-bold text-center font-eng-sub-font-1"
+        className="px-10 text-base font-bold text-center line-clamp-2 md:line-clamp-3 font-eng-sub-font-1"
         style={{
           color: `${getBrightness(color) ? "#121212" : "#fff"}`,
         }}
