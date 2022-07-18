7f7c069666123431da106887e13a2be4da40f482
MINJE-98 <jmj012100@gmail.com>
2022-07-18 15:03:05 +0900

    유저 프로필 컴포넌트 (#110)
    * fix: box이름을 headerbox로 변경
    
    * feat: TILOG_AUTH환경변수 추가
    
    * feat: getme 커스텀훅 작성
    
    * fix: 이벤트리스너로 리팩터링 및 로직 분리
    
    * fix: 로그아웃 로직 수정
    
    * feat: callback page 작성
    
    * fix: 헤더 디자인 수정
    
    * fix: 리덕스 삭제
    
    * fix: 리덕스 삭제
    
    * fix: code smell
    
    * fix: 이미지 컴포넌트 원자로 이동
    no-image 컴포넌트 작성
    인터페이스 작성
    
    * feat: 유저 이름 컴포넌트 작성
    인터페이스 작성
    
    * feat: 유저 프로필 컴포넌트 작성
    인터페이스 작성

---
+++ src/components/molecules/user/interface/profileUserProps.ts
@@ -0,0 +1,6 @@
+import { UserImageProps } from "@Components/atom/images/interface/userImageProps";
+import UserNameProp from "@Components/atom/user/interface/userNameProp";
+
+export default interface ProfileUserProps extends UserImageProps, UserNameProp {
+  UserImageStyle: string;
+}
