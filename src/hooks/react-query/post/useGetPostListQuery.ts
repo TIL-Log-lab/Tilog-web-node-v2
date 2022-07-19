7804bebe5ba06c6541700c48f20bd31d933bbf9c
MINJE-98 <jmj012100@gmail.com>
2022-07-19 15:19:55 +0900

    fix: 새로운 api에 맞게 리액트 쿼리 수정

--- src/hooks/react-query/post/useGetPostListQuery.ts
+++ src/hooks/react-query/post/useGetPostListQuery.ts
@@ -1,34 +1,43 @@
 import { AxiosResponse } from "axios";
 import { useInfiniteQuery } from "react-query";
 
-import { getPosts } from "@Api/adapter";
+import useGetCategoryListQuery from "@Hooks/react-query/category/useGetCategoryListQuery";
+import api from "@Library/api";
+import ExceptionInterface from "@Library/api/exception/interface";
 
-import ExceptionInterface from "@Api/errors/interfaces";
-import { GetPostsRequestDto } from "@Api/interface/post";
 import { GetPostsResponseDto } from "@til-log.lab/tilog-api";
 
-interface GetPostListQueryInterface extends GetPostsRequestDto {
-  queryKey: string;
+import GetPostRequestDto from "@Library/api/post/interface/GetPostRequestDto";
+
+interface GetPostListQueryInterface {
+  dateScope: GetPostRequestDto["dateScope"];
+  sortScope: GetPostRequestDto["sortScope"];
+  page: GetPostRequestDto["page"];
+  maxContent: GetPostRequestDto["maxContent"];
+  userId?: GetPostRequestDto["userId"];
+  categoryName?: string;
 }
 
 const useGetPostListQuery = ({
-  queryKey,
+  categoryName,
   dateScope,
   sortScope,
   maxContent,
   userId,
   page,
-  categoryId,
 }: GetPostListQueryInterface) => {
+  const { data } = useGetCategoryListQuery(categoryName);
+  const categoryId = !data ? 0 : data.data.list[0].id;
+
   return useInfiniteQuery<
     AxiosResponse<GetPostsResponseDto>,
     ExceptionInterface,
     AxiosResponse<GetPostsResponseDto>,
     string
   >(
-    queryKey,
+    `PostList-${categoryName}`,
     ({ pageParam = page }) => {
-      return getPosts(
+      return api.postService.getPosts(
         dateScope,
         sortScope,
         pageParam,
@@ -40,14 +49,19 @@ const useGetPostListQuery = ({
     {
       retry: 0,
       refetchOnWindowFocus: false,
-
+      refetchInterval: false,
+      retryOnMount: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60,
+      enabled: !!categoryId,
       getNextPageParam: (lastPages, pages) => {
         const nextPage = pages.length;
         const lastPagesListCount = lastPages.data.list.length;
 
-        if (lastPagesListCount != 0 && lastPagesListCount % maxContent === 0) {
+        if (lastPagesListCount !== 0 && lastPagesListCount % maxContent === 0) {
           return nextPage;
         }
+        return null;
       },
     }
   );
