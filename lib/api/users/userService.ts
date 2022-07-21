55813575353be532d27572ccd26e57383e573c5d
MINJE-98 <jmj012100@gmail.com>
2022-07-21 14:10:32 +0900

    feat: 유저 셋팅을 객체로 변환된 셋팅을 반환하는 로직 추가

--- lib/api/users/userService.ts
+++ lib/api/users/userService.ts
@@ -1,31 +1,50 @@
-import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { AxiosRequestConfig } from "axios";
 
 import validateToken from "@Library/api/auth/validateTokenDecorator";
-import ExceptionInterface from "@Library/api/exception/interface";
 import UserRepository from "@Library/api/users/userRepository";
+import userSettingsTransformObjectSettings from "@Library/api/utility/userSettingsTransformObjectSettings";
 
 import {
-  GetMeResponseDto,
-  GetUserProfileResponseDto,
   SetSettingRequestBodyDto,
   SetSettingRequestBodyDtoSettingTypeEnum,
 } from "@til-log.lab/tilog-api";
+
+import GetMeResponseTransFormSettingsDto from "@Library/api/users/interface/getMeResponseTransFormSettingsDto";
+import GetUserProfileResponseTransFormSettingsDto from "@Library/api/users/interface/getUserProfileResponseTransFormSettingsDto";
 
 export default class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
   @validateToken()
-  getMe(
-    options?: AxiosRequestConfig<ExceptionInterface>
-  ): Promise<AxiosResponse<GetMeResponseDto, ExceptionInterface>> {
-    return this.userRepository.usersControllerGetMe(options);
+  async getMe(
+    options?: AxiosRequestConfig<unknown>
+  ): Promise<GetMeResponseTransFormSettingsDto> {
+    const { data } = await this.userRepository.usersControllerGetMe(options);
+    const newResponse: GetMeResponseTransFormSettingsDto = {
+      id: data.userId,
+      avatar: data.avatar,
+      name: data.name,
+      createdAt: data.createdAt,
+      settings: userSettingsTransformObjectSettings(data.settings),
+    };
+    return newResponse;
   }
 
-  getUserProfile(
-    userId: number,
-    options?: AxiosRequestConfig<ExceptionInterface>
-  ): Promise<AxiosResponse<GetUserProfileResponseDto, ExceptionInterface>> {
-    return this.userRepository.usersControllerGetUserProfile(userId, options);
+  async getUserProfile(
+    userName: string,
+    options?: AxiosRequestConfig<unknown>
+  ): Promise<GetUserProfileResponseTransFormSettingsDto> {
+    const { data } = await this.userRepository.usersControllerGetUserProfile(
+      userName,
+      options
+    );
+    const newResponse: GetUserProfileResponseTransFormSettingsDto = {
+      avatar: data.avatar,
+      name: data.name,
+      createdAt: data.createdAt,
+      settings: userSettingsTransformObjectSettings(data.settings),
+    };
+    return newResponse;
   }
 
   @validateToken()
