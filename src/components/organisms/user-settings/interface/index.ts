54ff2437c625d5f7d4a16c5e770bdbf4660b2c65
MINJE-98 <jmj012100@gmail.com>
2022-07-22 20:28:17 +0900

    fix: 기존 컴포넌트 삭제

--- src/components/organisms/user-settings/interface/index.ts
+++
@@ -1,7 +0,0 @@
-import { SetSettingRequestBodyDtoSettingTypeEnum } from "@til-log.lab/tilog-api";
-
-export type UserSettingTypes = {
-  [x in SetSettingRequestBodyDtoSettingTypeEnum]: string | null;
-};
-
-export type UserSettingUnionTypes = keyof UserSettingTypes;
