1f94be0432d259524a4cd910b0d33fece53d8c97
MINJE-98 <jmj012100@gmail.com>
2022-07-24 20:50:29 +0900

    유저 셋팅 리팩터링 (#186)
    * refactor: summit 리팩터링
    
    form내에서 Button을 Summit으로 사용할 수 있기 때문에 button을 summit tpye으로 지정하고 버튼으로 변경하였습니다.
    
    재사용성을 높히기 위해 ButtonHTMLAttributes인터페이스를 확장하여 버튼에 관련된 props를 전달받아 사용할 수 있습니다.
    
    * refactor: SettingInput 리팩터링
    
    SettingInput으로 컴포넌트이름을 설정하게 되면 SettingInput일 경우에만 사용할 수 있기 때문에 TextInput으로 변경하였습니다.
    
    재사용성을 높히기 위해 InputHTMLAttributes을 확장하였습니다.
    
    React-form-hooks의 Props을 부모 컴포넌트로 부터 주입받아 재사용성을 높혔습니다.
    
    * fix: 필요없는 컴포넌트를 삭제하였습니다.
    
    * fix: 분자 단위 셋팅관련 TextInput 수정 반영
    
    * feat: useSettingForm 타입을 추가
    
    * feat: handleSummit 분리
    
    * fix: 하위요소 수정사항 반영
    
    * fix: 리다이렉트 URL 수정
    
    * fix: 사용하지 않는 코드 제거
    
    * fix: 헤더 설정 리다이렉트 주소 변경

--- src/components/atom/text/UserName.tsx
+++
@@ -1,9 +0,0 @@
-interface UserNameProp {
-  children: string;
-  className?: string;
-}
-const UserName = ({ className, children }: UserNameProp) => (
-  <span className={`${className}`}>{children}</span>
-);
-
-export default UserName;
--- src/components/organisms/form/settings/UserSettingForm.tsx
+++ src/components/organisms/form/settings/UserSettingForm.tsx
@@ -1,20 +1,15 @@
-import { FormProvider, SubmitHandler } from "react-hook-form";
-import toast from "react-hot-toast";
+import { FormProvider } from "react-hook-form";
 
-import SummitInput from "@Components/atom/input/SummitInput";
+import SummitButton from "@Components/atom/button/SummitButton";
 import DisplayNameInput from "@Components/molecules/input/settings/DisplayNameInput";
 import EmailInput from "@Components/molecules/input/settings/EmailInput";
 import IntroductionInput from "@Components/molecules/input/settings/IntroductionInput";
 import PositionInput from "@Components/molecules/input/settings/PositionInput";
+import useHandleSummit from "@Components/organisms/form/settings/hooks/useHandleSummit";
 import useSettingForm from "@Components/organisms/form/settings/hooks/useSettingForm";
 import SUMMIT from "@Constants/text/summit";
-import useSetUserSetting from "@Hooks/react-query/userSetting/useSetUserSetting";
-import SAVE_MESSAGE from "@Library/messages/constants/success/save";
-
-import { SetSettingRequestBodyDtoSettingTypeEnum } from "@til-log.lab/tilog-api";
 
 import GetMeResponseTransFormSettingsDto from "@Library/api/users/interface/getMeResponseTransFormSettingsDto";
-import UserSettingTypes from "@Library/api/users/interface/userSettingTypes";
 
 const UserSettingForm = ({
   userInfo,
@@ -22,43 +17,21 @@ const UserSettingForm = ({
   userInfo: GetMeResponseTransFormSettingsDto;
 }) => {
   const method = useSettingForm(userInfo.settings);
-  const { mutate } = useSetUserSetting();
-  // NOTE: 설정 제출 이벤트
-  const onSubmit: SubmitHandler<UserSettingTypes> = async (object) => {
-    const loading = toast.loading("로딩중...");
-    Object.keys(object).forEach((key) => {
-      const userSettingKey = key as SetSettingRequestBodyDtoSettingTypeEnum;
-      mutate(
-        {
-          content: object[userSettingKey],
-          settingType: userSettingKey,
-        },
-        {
-          onError: (e) => {
-            toast.error(e.message.ko, {
-              id: loading,
-            });
-          },
-          onSuccess: () => {
-            toast.success(SAVE_MESSAGE.ko, {
-              id: loading,
-            });
-          },
-        }
-      );
-    });
-  };
+  const handleSubmit = useHandleSummit();
+
   return (
     <FormProvider {...method}>
       <form
-        className="relative borer p-14 bg-neutral-100 dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700"
-        onSubmit={method.handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-10 borer p-14 bg-neutral-100 dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700"
+        onSubmit={method.handleSubmit(handleSubmit)}
       >
         <DisplayNameInput />
         <EmailInput />
         <IntroductionInput />
         <PositionInput />
-        <SummitInput value={SUMMIT.ko} />
+        <div className="text-right">
+          <SummitButton text={SUMMIT.ko} />
+        </div>
       </form>
     </FormProvider>
   );
