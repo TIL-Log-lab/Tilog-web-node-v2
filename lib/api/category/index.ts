d1e226dd3d835920274e01cf7d0e34733130d9dc
MINJE-98 <jmj012100@gmail.com>
2022-07-24 09:31:38 +0900

    포스트, 카테고리 api 작성 (#140)
    * feat: 카테고리 api 작성
    
    * feat: 포스트 api 작성
    
    * fix: 새로운 api에 맞게 리액트 쿼리 수정
    
    * fix: 새로운 api에 맞게 리액트 쿼리 수정
    
    * fix: 새로운 api에 맞게 리액트 쿼리 수정
    
    * fix: code smell
    
    * fix: defaultvalue 삭제
    
    * feat: postList 쿼리를 카테고리 기준으로 분리
    
    * feat: post, category 서비스 내보내기

---
+++ lib/api/category/index.ts
@@ -0,0 +1,12 @@
+import { TILOG_API } from "@Constants/environment";
+import CategoryRepository from "@Library/api/category/categoryRepository";
+import CategoryService from "@Library/api/category/categoryService";
+import httpClient from "@Library/api/httpClient";
+
+const categoryRepository = new CategoryRepository(httpClient.http, TILOG_API);
+const categoryService = new CategoryService(
+  categoryRepository,
+  httpClient.http
+);
+
+export default categoryService;
