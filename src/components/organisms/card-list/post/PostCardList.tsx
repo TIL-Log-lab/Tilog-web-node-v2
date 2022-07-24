a01c4f466dae616973b7d1fd7daea105b1cd3aa3
MINJE-98 <jmj012100@gmail.com>
2022-07-24 09:59:03 +0900

    fix: 로딩 컴포넌트 이름변경

--- src/components/organisms/card-list/post/PostCardList.tsx
+++ src/components/organisms/card-list/post/PostCardList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import CardLoading from "@Components/molecules/card/Loading";
+import CardLoading from "@Components/molecules/card/CardLoading";
 import PostCard from "@Components/molecules/card/post/PostCard";
 import useGetPostListQuery from "@Hooks/react-query/post/useGetPostListQuery";
 
