81c1f966da3a9e814a4f47a5b5b9a9428d0adb9b
MINJE-98 <jmj012100@gmail.com>
2022-07-24 19:12:36 +0900

    fix: 사용하지 않는 코드 제거

--- src/components/atom/text/DisplayName.tsx
+++
@@ -1,8 +0,0 @@
-interface DisplayNameProps {
-  className: string;
-  children: string;
-}
-const DisplayName = ({ className, children }: DisplayNameProps) => (
-  <h3 className={`${className}`}>{children}</h3>
-);
-export default DisplayName;
