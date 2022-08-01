f2bdac1cab3bc335f7b3f51dcd36cd4771fe8100
MINJE-98 <jmj012100@gmail.com>
2022-08-01 09:54:47 +0900

    feat: add text, image input

---
+++ src/components/atom/input/ImageInput.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes } from "react";
+
+import { Path, UseFormRegister } from "react-hook-form";
+
+interface ImageInputProps<TFormValues>
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  inputName: Path<TFormValues>;
+  register: UseFormRegister<TFormValues>;
+}
+const ImageInput = <TFormValues extends unknown>({
+  register,
+  inputName,
+  id = "fileInput",
+}: ImageInputProps<TFormValues>) => {
+  return (
+    <input
+      id={id}
+      type="file"
+      className="hidden"
+      accept="image/*"
+      {...register(inputName)}
+    />
+  );
+};
+
+export default ImageInput;
