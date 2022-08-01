6ecba28450ad9049ffc68e8f78708246ea7d1e96
MINJE-98 <jmj012100@gmail.com>
2022-08-01 14:06:05 +0900

    fix: code smell

--- src/hooks/react-query/category/useSearchCategory.ts
+++ src/hooks/react-query/category/useSearchCategory.ts
@@ -5,7 +5,7 @@ import { GetCategoriesItem } from "@til-log.lab/tilog-api";
 const useSearchCategory = () => {
   const { data } = useAllGetCategoryListQuery();
 
-  const searchCategory = (
+  return (
     categoryName?: GetCategoriesItem["categoryName"],
     categoryId?: GetCategoriesItem["id"]
   ): GetCategoriesItem[] | null => {
@@ -13,22 +13,15 @@ const useSearchCategory = () => {
     const categoryList = data.data.list;
 
     if (categoryName) {
-      return categoryList.filter((v) => {
-        const filtered = v.categoryName.includes(categoryName);
-        return filtered;
-      });
+      return categoryList.filter((v) => v.categoryName.includes(categoryName));
     }
 
     if (categoryId) {
-      return categoryList.filter((v) => {
-        const filtered = v.id === categoryId;
-        return filtered;
-      });
+      return categoryList.filter((v) => v.id === categoryId);
     }
 
     return categoryList;
   };
-  return searchCategory;
 };
 
 export default useSearchCategory;
