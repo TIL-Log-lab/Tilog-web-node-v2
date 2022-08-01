0efabdc76428362651acdbb9d4bf2c7f4ff778e5
MINJE-98 <jmj012100@gmail.com>
2022-08-01 14:57:29 +0900

    fix: 원자 단위 버튼 삭제

--- src/components/organisms/form/writer/Writer.tsx
+++ src/components/organisms/form/writer/Writer.tsx
@@ -1,6 +1,7 @@
 import { FormProvider, useForm } from "react-hook-form";
 
-import SummitButton from "@Components/atom/button/SummitButton";
+// import SummitButton from "@Components/atom/button/SummitButton";
+
 import CoverImage from "@Components/molecules/images/writer/CoverImage";
 import SearchCategory from "@Components/molecules/input/writer/category-search";
 import SubTitleInput from "@Components/molecules/input/writer/SubTitleInput";
@@ -22,15 +23,19 @@ const Writer = () => {
         <SearchCategory />
         <TiptapEditor editable />
         <div className="flex gap-x-2">
-          <SummitButton
+          <button
+            type="submit"
             onClick={() => method.setValue("isPrivate", false)}
-            text="게시하기"
-          />
-          <SummitButton
+            className="w-40 h-12 rounded-md text-neutral-50 bg-neutral-800"
+          >
+            게시하기
+          </button>
+          <button
             onClick={() => method.setValue("isPrivate", true)}
-            className="w-40 h-12 text-neutral-800 hover:bg-neutral-300"
-            text="비밀로 게시하기"
-          />
+            className="w-40 h-12 rounded-md text-neutral-800 hover:bg-neutral-300"
+          >
+            비밀로 게시하기
+          </button>
         </div>
       </form>
     </FormProvider>
