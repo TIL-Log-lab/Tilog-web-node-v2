9a305e419a79f7d1b1abad8fede7c25354a5c487
MINJE-98 <jmj012100@gmail.com>
2022-08-02 21:11:43 +0900

    fix: Atom 단위가 아닌 컴포넌트 제거

--- src/components/atom/text/Title.tsx
+++
@@ -1,9 +0,0 @@
-interface TitleProps {
-  className: string;
-  children: string;
-}
-
-const Title = ({ className, children }: TitleProps) => (
-  <h1 className={`${className}`}>{children}</h1>
-);
-export default Title;
