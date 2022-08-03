5e7a9bc99d8f2416bf57f37c3af39ac9cdef0b67
MINJE-98 <jmj012100@gmail.com>
2022-08-04 08:38:37 +0900

    feat: setting page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,8 +1,8 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 
-import UserSettingsForm from "@Components/organisms/form/settings/UserSettingForm";
-import Header from "@Components/organisms/Header";
+import Header from "@Components/organisms/header";
+import Settings from "@Components/organisms/settings";
 import useGetMeQuery from "@Hooks/react-query/user/useGetMeQuery";
 
 const SettingPage: NextPage = () => {
@@ -15,7 +15,7 @@ const SettingPage: NextPage = () => {
     <div>
       <Header />
       <div className="flex m-auto max-w-[1280px] items-center mt-10 justify-center px-5">
-        {userInfo.isSuccess && <UserSettingsForm userInfo={userInfo.data} />}
+        {userInfo.isSuccess && <Settings userInfo={userInfo.data} />}
       </div>
     </div>
   );
