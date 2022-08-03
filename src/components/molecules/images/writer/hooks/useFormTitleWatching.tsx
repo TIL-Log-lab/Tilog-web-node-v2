9fba2c59aa1259266e25f6bc349625fdd502515c
MINJE-98 <jmj012100@gmail.com>
2022-08-03 12:37:44 +0900

    fix: 필요없는 코드 삭제

--- src/components/molecules/images/writer/hooks/useFormTitleWatching.tsx
+++
@@ -1,9 +0,0 @@
-import { useFormContext } from "react-hook-form";
-
-import { CreatePostRequestBodyDto } from "@til-log.lab/tilog-api";
-
-const useFormTitleWatching = () => {
-  const { watch } = useFormContext<CreatePostRequestBodyDto>();
-  return watch("title");
-};
-export default useFormTitleWatching;
