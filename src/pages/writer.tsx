6b1c8f29b4fc11e59290e2170402960553b24272
MINJE-98 <jmj012100@gmail.com>
2022-08-03 23:01:50 +0900

    fix: writerpage로 이름 변경

--- src/pages/writer.tsx
+++ src/pages/writer.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 
-import Writer from "@Components/organisms/form/writer/Writer";
+import Writer from "@Components/organisms/writer";
 
-const EditorPage: NextPage = () => {
+const WriterPage: NextPage = () => {
   return (
     <div className="m-auto max-w-[1000px] h-full p-10 items-center px-5">
       <Writer />
@@ -10,4 +10,4 @@ const EditorPage: NextPage = () => {
   );
 };
 
-export default EditorPage;
+export default WriterPage;
