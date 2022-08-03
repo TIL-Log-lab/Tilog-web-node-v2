5fad83666206953f1efe00138f4d48ecb74f97c0
MINJE-98 <jmj012100@gmail.com>
2022-08-04 08:36:51 +0900

    feat: setting constants

---
+++ constants/text/settings/index.ts
@@ -0,0 +1,8 @@
+import SUMMIT from "./summit";
+import WRONG_EMAIL from "./wrongEmail";
+
+export * from "./desc";
+export * from "./title";
+export * from "./type";
+
+export { SUMMIT, WRONG_EMAIL };
