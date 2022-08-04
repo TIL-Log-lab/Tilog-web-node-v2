b6e773b2b0f8edf36a1bb60e3b26442b680abed0
MINJE-98 <jmj012100@gmail.com>
2022-08-04 15:44:11 +0900

    fix: allusercategory query key tnwjd

--- src/hooks/react-query/category/useAllGetUserCategoryListQuery.ts
+++ src/hooks/react-query/category/useAllGetUserCategoryListQuery.ts
@@ -1,10 +1,19 @@
+import { AxiosResponse } from "axios";
 import { useQuery } from "react-query";
 
 import api from "@Library/api";
 
+import { GetUserCategoriesResponseDto } from "@til-log.lab/tilog-api";
+
+import ExceptionInterface from "@Library/api/exception/interface";
+
 const useAllGetUserCategoryListQuery = (userId: number) => {
-  return useQuery(
-    "userCategoryList",
+  return useQuery<
+    AxiosResponse<GetUserCategoriesResponseDto>,
+    ExceptionInterface,
+    AxiosResponse<GetUserCategoriesResponseDto>
+  >(
+    ["userCategoryList", userId],
     () => api.categoryService.getUsersCategories(userId),
     {
       retry: 0,
