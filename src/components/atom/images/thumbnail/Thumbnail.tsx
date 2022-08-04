2e6b74f3d66cbeb55863585fce2e7fcff6a6c3e6
MINJE-98 <jmj012100@gmail.com>
2022-08-04 12:30:39 +0900

    style: z-index 수정

--- src/components/atom/images/thumbnail/Thumbnail.tsx
+++ src/components/atom/images/thumbnail/Thumbnail.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const ThumbNail = ({ thumbnailUrl }: { thumbnailUrl: string }) => (
-  <div className="relative w-full h-full -z-10">
+  <div className="relative z-0 w-full h-full">
     <Image layout="fill" alt="thumbnail" src={thumbnailUrl} />
   </div>
 );
