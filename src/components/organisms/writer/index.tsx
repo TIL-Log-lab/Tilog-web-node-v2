7add4d7c2f0427fb6e9f97dd61a8dadc88bd051b
MINJE-98 <jmj012100@gmail.com>
2022-08-07 00:43:00 +0900

    fix: button 수정 반영

--- src/components/organisms/writer/index.tsx
+++ src/components/organisms/writer/index.tsx
@@ -1,7 +1,5 @@
 import { FormProvider, useForm } from "react-hook-form";
 
-import PrimarySubmit from "@Components/atom/buttons/PrimarySubmit";
-import SecondarySubmit from "@Components/atom/buttons/SecondarySubmit";
 import TiptapEditor from "@Components/atom/text-area/TiptapEditor";
 import useHandleSummit from "@Components/organisms/writer/hooks/useHandleSummit";
 import SearchCategory from "@Components/organisms/writer/input/category-search";
@@ -15,6 +13,8 @@ import {
 } from "@Constants/text/writer";
 
 import { CreatePostRequestBodyDto } from "@til-log.lab/tilog-api";
+import PrimaryButton from "@Components/atom/buttons/PrimaryButton";
+import SecondaryButton from "@Components/atom/buttons/SecondaryButton";
 
 const Writer = () => {
   const method = useForm<CreatePostRequestBodyDto>();
@@ -28,18 +28,20 @@ const Writer = () => {
         <SearchCategory />
         <TiptapEditor />
         <div className="flex justify-end gap-x-2">
-          <SecondarySubmit
+          <SecondaryButton
+            type="submit"
             onClick={() => method.setValue(IS_PRIVATE, true)}
             className="w-32 mt-2 hover:text-neutral-50 dark:text-neutral-50 text-neutral-400"
           >
             <span className="">{PUBLISH_PRIVATE_POST.ko}</span>
-          </SecondarySubmit>
-          <PrimarySubmit
+          </SecondaryButton>
+          <PrimaryButton
+            type="submit"
             onClick={() => method.setValue(IS_PRIVATE, false)}
             className="w-32 mt-2"
           >
             <span className="text-neutral-50">{PUBLISH_POST.ko}</span>
-          </PrimarySubmit>
+          </PrimaryButton>
         </div>
       </form>
     </FormProvider>
