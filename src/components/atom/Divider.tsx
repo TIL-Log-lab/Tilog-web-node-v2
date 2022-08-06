e58e8eb3330c74fb2ab8f65a00bf9055c5cbe633
MINJE-98 <jmj012100@gmail.com>
2022-08-07 00:43:29 +0900

    style: divider 스타일 수정

--- src/components/atom/Divider.tsx
+++ src/components/atom/Divider.tsx
@@ -3,6 +3,8 @@ import { AllHTMLAttributes } from "react";
 interface DividerProps extends AllHTMLAttributes<HTMLHRElement> {}
 
 const Divider = ({ className = "w-full my-5" }: DividerProps) => {
-  return <hr className={`${className} border-neutral-300`} />;
+  return (
+    <hr className={`${className} border-neutral-300 dark:border-neutral-700`} />
+  );
 };
 export default Divider;
