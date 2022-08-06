c84959936faef900cb9942ab56192b04d578a8d1
MINJE-98 <jmj012100@gmail.com>
2022-08-07 00:43:22 +0900

    style: overlay 스타일 수정

--- src/components/atom/overlay/OpacityOverlay.tsx
+++ src/components/atom/overlay/OpacityOverlay.tsx
@@ -2,7 +2,10 @@ import { HTMLAttributes } from "react";
 
 const OpacityOverlay = ({ ...props }: HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div {...props} className="absolute w-full h-full bg-black opacity-50" />
+    <div
+      {...props}
+      className="absolute w-full h-full opacity-50 bg-neutral-900 dark:bg-neutral-700"
+    />
   );
 };
 
