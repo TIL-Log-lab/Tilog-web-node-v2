295eb7f4c4c65ea0ddf01898d42061efefea9062
MINJE-98 <jmj012100@gmail.com>
2022-08-07 13:59:02 +0900

    fix: divider 삭제, global style에 적용

--- src/components/atom/Divider.tsx
+++
@@ -1,8 +0,0 @@
-import { AllHTMLAttributes } from "react";
-
-interface DividerProps extends AllHTMLAttributes<HTMLHRElement> {}
-
-const Divider = ({ className = "w-full my-5" }: DividerProps) => {
-  return <hr className={`${className} border-neutral-300`} />;
-};
-export default Divider;
