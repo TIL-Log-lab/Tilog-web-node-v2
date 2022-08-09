7959ba3dd2d294ca04f5a0ae416721b7f36da483
MINJE-98 <jmj012100@gmail.com>
2022-08-09 19:16:35 +0900

    feat(constants-post): card title

---
+++ constants/post/card/title.ts
@@ -0,0 +1,9 @@
+import LANGUAGE from "@Library/language/constant";
+
+import { LanguageInterface } from "@Library/language/interface";
+
+const POSTCARD_ALL_CATEGORY_TITLE: LanguageInterface = {
+  [LANGUAGE.ko]: "전체 카테고리",
+  [LANGUAGE.en]: "All Categories",
+};
+export default POSTCARD_ALL_CATEGORY_TITLE;
