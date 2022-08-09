167f30de96a8ef2521e000477b59898f8e5c9b21
MINJE-98 <jmj012100@gmail.com>
2022-08-09 20:25:33 +0900

    feat(modules): header

---
+++ src/components/modules/post/PostHeader.tsx
@@ -0,0 +1,35 @@
+import RenderTechIcons from "@Components/common/molecules/tech-icons/RenderTechIcons";
+import DateUserProfile from "@Components/common/organisms/profile/DateUserProfile";
+import PostUpdateLink from "@Components/modules/post/link/postUpdateLink";
+import useIsOwner from "@Hooks/useIsOwner";
+
+import { GetPostDetailResponseDto } from "@til-log.lab/tilog-api";
+
+interface PostHeaderProps {
+  post: GetPostDetailResponseDto;
+}
+
+const PostHeader = ({ post }: PostHeaderProps) => {
+  const isOwner = useIsOwner(post.user.userId);
+  return (
+    <header>
+      <div className="flex">
+        <DateUserProfile userId={post.user.userId} createdAt={post.createdAt} />
+        {isOwner && (
+          <div className="flex ml-auto space-x-5">
+            <PostUpdateLink postId={post.id} />
+          </div>
+        )}
+      </div>
+      <div className="my-3 text-center">
+        <div className="text-2xl">
+          <RenderTechIcons categoryName={post.category.name} />
+        </div>
+        <h1>{post.title}</h1>
+        <h4>{post.subTitle}</h4>
+      </div>
+    </header>
+  );
+};
+
+export default PostHeader;
