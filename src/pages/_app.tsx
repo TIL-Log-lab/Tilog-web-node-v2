5dadf7be0890b952bfea99da0221d1c14c20eb59
MINJE-98 <jmj012100@gmail.com>
2022-08-09 18:37:55 +0900

    feat(_app): header 컴포넌트 (#302)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,15 @@ import { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import Header from "@Components/common/organisms/header";
+
 const queryClient = new QueryClient({});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
+      <Header />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
