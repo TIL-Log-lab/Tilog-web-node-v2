effdeca95404d66d04f4f6c7b2a3153f855e321c
MINJE-98 <jmj012100@gmail.com>
2022-08-09 21:18:09 +0900

    fix: 필요하지 않는 코드 삭제 (#309)

--- src/components/modules/post/index.tsx
+++ src/components/modules/post/index.tsx
@@ -1,5 +1,5 @@
 import IconCategoryLink from "@Components/modules/post/link/IconCategoryLink";
-import PostUpdateLink from "@Components/modules/post/link/postUpdateLink";
+import PostUpdateLink from "@Components/modules/post/link/PostUpdateLink";
 import PostHeader from "@Components/modules/post/PostHeader";
 import PostLike from "@Components/modules/post/PostLike";
 import PostWriter from "@Components/modules/post/PostWriter";
