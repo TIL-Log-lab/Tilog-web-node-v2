d06f8e77c16daea8ea5503ec113ad1c9b2623c91
MINJE-98 <jmj012100@gmail.com>
2022-08-09 20:26:14 +0900

    feat(hooks-like): like mutation, hasLike query

--- src/hooks/react-query/like/useLikeMutation.tsx
+++ src/hooks/react-query/like/useLikeMutation.tsx
@@ -1,35 +1,25 @@
-import toast from "react-hot-toast";
 import { AxiosResponse } from "axios";
-import { useSelector } from "react-redux";
-import { useMutation } from "react-query";
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "react-query";
 
-import { languageSelector } from "@Redux/userInfo";
-import { setLike, unSetLike } from "@Api/adapter/postLike";
-import clientSideAuthentication from "@Auth/clientSideAuthentication";
+import api from "@Library/api";
 
-import ExceptionInterface from "@Api/errors/interfaces";
+import ExceptionInterface from "@Library/api/exception/interface";
+import LikedRequestDto from "@Library/api/post/like/interface/LikedRequestDto";
 
 export default function useLikeMutation() {
-  const language = useSelector(languageSelector);
-  const likeMutation = useMutation<
-    Promise<AxiosResponse<void>>,
+  const queryClient = useQueryClient();
+  return useMutation<
+    AxiosResponse<void, ExceptionInterface>,
     ExceptionInterface,
     string
-  >((postId: string) => clientSideAuthentication(() => setLike(postId)), {
-    onError: (error) => {
-      toast.error(error.message[language]);
+  >((postId: LikedRequestDto) => api.postLikeService.toggleLike(postId), {
+    onError: (err) => {
+      toast.error(err.message.ko);
+    },
+    onSuccess(_data, variables) {
+      queryClient.refetchQueries(["post-detail", variables]);
+      queryClient.refetchQueries(["like", variables]);
     },
   });
-
-  const unLikeMutation = useMutation<
-    Promise<AxiosResponse<void>>,
-    ExceptionInterface,
-    string
-  >((postId: string) => clientSideAuthentication(() => unSetLike(postId)), {
-    onError: (error) => {
-      toast.error(error.message[language]);
-    },
-  });
-
-  return [likeMutation, unLikeMutation];
 }
