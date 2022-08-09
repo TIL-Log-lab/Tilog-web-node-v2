c2800b6bbc61b245786824edf19f5dc938656983
MINJE-98 <jmj012100@gmail.com>
2022-08-09 21:16:58 +0900

    fix: 필요하지 않는 코드 삭제

--- src/hooks/react-query/editor/useEditorMutation.ts
+++
@@ -1,8 +0,0 @@
-import { createPost } from "@Api/adapter";
-import { useAuthMutation } from "@Query/auth-wrapper/useAuthMutation";
-import { CreatePostRequestBodyDto } from "@til-log.lab/tilog-api";
-
-const useEditorMutation = () => {
-  return useAuthMutation<CreatePostRequestBodyDto>((data) => createPost(data));
-};
-export default useEditorMutation;
