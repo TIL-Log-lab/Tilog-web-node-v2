b4a48450fa27bd0349bea21b3e725117830607af
MINJE-98 <jmj012100@gmail.com>
2022-08-11 13:54:48 +0900

    feat(constants): viewCount 텍스트 작성

---
+++ constants/post/profile/viewCount.ts
@@ -0,0 +1,8 @@
+import LANGUAGE from "@Library/language/constant";
+
+import { LanguageInterface } from "@Library/language/interface";
+
+export const VIEW_COUNT: LanguageInterface = {
+  [LANGUAGE.ko]: "조회수",
+  [LANGUAGE.en]: "Views",
+};
