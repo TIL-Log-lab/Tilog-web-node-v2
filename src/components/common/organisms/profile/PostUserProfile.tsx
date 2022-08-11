04900c75cfe9125c7304deba37f34863288ddd72
MINJE-98 <jmj012100@gmail.com>
2022-08-11 13:54:19 +0900

    refactor(organisms): DateUserProfile-> UserProfile 이름 변경 및 UserProfile 수정 반영

--- src/components/common/organisms/profile/PostUserProfile.tsx
+++ src/components/common/organisms/profile/PostUserProfile.tsx
@@ -1,27 +1,32 @@
+import { VscCircleFilled } from "react-icons/vsc";
+
 import LockIcon from "@Components/common/atom/icons/LockIcon";
 import LinkTo from "@Components/common/atom/LinkTo";
-import UserProfile from "@Components/common/molecules/profile/UserProfile";
+import ProfileImage from "@Components/common/molecules/images/ProfileImage";
 import DateFnsFormatter from "@Components/common/molecules/text/DateFnsFormatter";
+import { VIEW_COUNT } from "@Constants/post";
 import useGetUserProfileQuery from "@Hooks/react-query/user/useGetUserProfileQuery";
 
-interface DateUserProfileProps {
+interface UserProfileProps {
   userId: number;
   createdAt: string;
+  viewCount: number;
   isPrivate?: boolean;
 }
 
-const DateUserProfile = ({
+const UserProfile = ({
   userId,
   createdAt,
+  viewCount,
   isPrivate,
-}: DateUserProfileProps) => {
+}: UserProfileProps) => {
   const userInfo = useGetUserProfileQuery(userId);
 
   if (userInfo.isError) return <p>{userInfo.error.message.ko}</p>;
   if (userInfo.isLoading)
     return (
       <div className="flex items-center space-x-3">
-        <UserProfile className="w-10 h-10" />
+        <ProfileImage className="w-10 h-10" />
         <span className="text-sm font-bold">로딩중..</span>
         <DateFnsFormatter className="line-clamp-1" date={createdAt} />
       </div>
@@ -30,7 +35,7 @@ const DateUserProfile = ({
   return (
     <div className="flex items-center space-x-3">
       <LinkTo href={`/blog/${userInfo.data.name}`}>
-        <UserProfile className="w-10 h-10" avatar={userInfo.data.avatar} />
+        <ProfileImage className="w-10 h-10" avatar={userInfo.data.avatar} />
       </LinkTo>
       <div>
         <LinkTo href={`/blog/${userInfo.data.name}`}>
@@ -41,10 +46,18 @@ const DateUserProfile = ({
             {isPrivate && <LockIcon />}
           </span>
         </LinkTo>
-        <DateFnsFormatter className="text-xs line-clamp-1" date={createdAt} />
+        <div>
+          <div className="space-x-1">
+            <DateFnsFormatter className="inline text-xs" date={createdAt} />
+            <VscCircleFilled className="inline text-[6px] text-neutral-400" />
+            <p className="inline text-xs">
+              {VIEW_COUNT.ko} {viewCount}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default DateUserProfile;
+export default UserProfile;
