7fbc85f7c949aeae0d94c430f56c56ffbd920d20
MINJE-98 <jmj012100@gmail.com>
2022-08-13 00:21:40 +0900

    feat(constants): intro, cardTitle 텍스트 상수

--- constants/post/card/title.ts
+++ constants/post/card/title.ts
@@ -11,3 +11,18 @@ export const MOST_POPULAR_POSTCARD: LanguageInterface = {
   [LANGUAGE.ko]: "TILog의 인기 게시글",
   [LANGUAGE.en]: "TILog's Popular Post",
 };
+
+export const CATEGORY_MOST_POPULAR_POSTCARD: LanguageInterface = {
+  [LANGUAGE.ko]: "의 인기 게시글",
+  [LANGUAGE.en]: "'s Popular Post",
+};
+
+export const LATEST_POSTCARD: LanguageInterface = {
+  [LANGUAGE.ko]: "TILog의 최신 게시글",
+  [LANGUAGE.en]: "'s Latest Post",
+};
+
+export const LATEST_CATEGORY_POSTCARD: LanguageInterface = {
+  [LANGUAGE.ko]: "의 최신 게시글",
+  [LANGUAGE.en]: "'s Popular Post",
+};
