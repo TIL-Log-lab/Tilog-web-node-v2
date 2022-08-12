85998ed0b4909ff7992e8b840951ee4d6fb16a6b
MINJE-98 <jmj012100@gmail.com>
2022-08-12 22:51:23 +0900

    feat(common-atom): CardTitle 추가

---
+++ src/components/common/atom/CardTitle.tsx
@@ -0,0 +1,17 @@
+import { HTMLAttributes } from "react";
+
+interface CardTitleProps extends HTMLAttributes<HTMLHeadingElement> {
+  title: string;
+}
+
+const CardTitle = ({ title, children, ...props }: CardTitleProps) => {
+  return (
+    <div className="flex justify-between">
+      <h4 {...props} className="text-neutral-600 dark:text-neutral-300">
+        {title}
+      </h4>
+      {children}
+    </div>
+  );
+};
+export default CardTitle;
