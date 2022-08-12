ea7a88dbee01c90bccf34adb1787ef8b2f59bef4
MINJE-98 <jmj012100@gmail.com>
2022-08-12 22:28:15 +0900

    style(atom-buttons): 내부 padding값 수정

--- src/components/common/atom/buttons/SecondaryButton.tsx
+++ src/components/common/atom/buttons/SecondaryButton.tsx
@@ -13,7 +13,7 @@ const SecondaryButton = ({
   return (
     <button
       type={!isSubmit ? "button" : "submit"}
-      className={`${className} p-2.5 rounded-md w-full h-fit hover:bg-neutral-200 dark:hover:bg-neutral-700 text-neutral-800 dark:text-neutral-50 hover:text-neutral-50  font-eng-sub-font-2`}
+      className={`${className} py-2.5 px-6 rounded-md w-full h-fit hover:bg-neutral-200 dark:hover:bg-neutral-700 text-neutral-800 dark:text-neutral-50 hover:text-neutral-50  font-eng-sub-font-2`}
       {...props}
     >
       {children}
