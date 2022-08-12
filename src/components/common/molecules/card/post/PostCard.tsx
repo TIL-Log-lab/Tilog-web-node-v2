0c14fa7e174ce4d1f85cb8e3f1506442c54422a0
MINJE-98 <jmj012100@gmail.com>
2022-08-12 22:39:48 +0900

    [스타일 수정] PostCard, PostCardList  (#325)
    * style(molecules-card): 위치값, 넓이 스타일 수정
    
    * fix(organisms-list): 기존 카드 타이틀 삭제

--- src/components/common/molecules/card/post/PostCard.tsx
+++ src/components/common/molecules/card/post/PostCard.tsx
@@ -10,7 +10,7 @@ const PostCard = ({ post }: { post: GetPostsItem }) => {
   return (
     <div
       key={post.id}
-      className="flex flex-col md:flex-row bg-neutral-100 dark:bg-neutral-800 md:w-[550px] xl:w-[650px] max-w-[450px] md:max-w-[650px] w-full"
+      className="flex flex-col lg:flex-row bg-neutral-100 dark:bg-neutral-800 lg:max-w-[600px] w-full"
     >
       <div className="w-full p-4 md:py-4 md:pl-4">
         <PostUserProfile
@@ -20,7 +20,7 @@ const PostCard = ({ post }: { post: GetPostsItem }) => {
           createdAt={post.createdAt}
         />
 
-        <div className="mt-2 md:h-[120px] h-[100px]">
+        <div className="mt-2 h-[95px]">
           <LinkTo href={`/post/${post.id}`}>
             <h5 className="font-bold line-clamp-2 text-neutral-800 dark:text-neutral-50">
               {post.title}
@@ -37,7 +37,7 @@ const PostCard = ({ post }: { post: GetPostsItem }) => {
           <LikeCounter count={post.like} />
         </div>
       </div>
-      <div className="order-first w-full md:order-last">
+      <div className="order-first w-full lg:order-last">
         <LinkTo href={`/post/${post.id}`} className="w-full hover:no-underline">
           <PostThumbnail
             id={post.id}
