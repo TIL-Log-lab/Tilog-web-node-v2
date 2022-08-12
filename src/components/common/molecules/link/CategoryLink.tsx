afd501521889b1aaa58b47bd63dc093d9d2af97d
MINJE-98 <jmj012100@gmail.com>
2022-08-12 22:41:34 +0900

    style(molecules-link): font, color 수정

--- src/components/common/molecules/link/CategoryLink.tsx
+++ src/components/common/molecules/link/CategoryLink.tsx
@@ -13,9 +13,12 @@ const CategoryLink = ({ categoryName }: CategoryLinkProps) => {
         pathname: "/search",
         query: { category: categoryName },
       }}
-      className={`underline decoration-${categoryName} text-sm font-semibold text-${categoryName} font-eng-sub-font-1`}
+      className={`underline decoration-${categoryName} text-xs font-semibold text-${categoryName}`}
     >
-      #<span className="text-xs">{categoryName}</span>
+      #{" "}
+      <span className="text-xs text-neutral-500 hover:text-neutral-800 dark:hover:text-neutral-50">
+        {categoryName}
+      </span>
     </LinkTo>
   );
 };
