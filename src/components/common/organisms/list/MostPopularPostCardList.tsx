458e51adea29b2dd017973cef8f6e0f7cbf593c7
MINJE-98 <jmj012100@gmail.com>
2022-08-13 00:24:03 +0900

    fix(organisms): MostPopularPostCardList
    페이징 렌더링 방식을 없애고 일반 렌더링 방식으로 수정

--- src/components/common/organisms/list/MostPopularPostCardList.tsx
+++ src/components/common/organisms/list/MostPopularPostCardList.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 
-import CardLoading from "@Components/common/molecules/card/CardLoading";
 import MostPopularPostCard from "@Components/common/molecules/card/post/MostPopularPostCard";
-import { MOST_POPULAR_POSTCARD } from "@Constants/post";
 import useGetMostPopularPostListQuery from "@Hooks/react-query/post/useGetMostPopularPostListQuery";
+import useGetStringTypeToRouter from "@Hooks/router/useGetStringTypeToRouter";
+import isArrayEmpty from "@Library/utility/isArrayEmpty";
 
 import GetPostRequestDto from "@Library/api/post/interface/getPostRequestDto";
 
 interface MostPopularPostCardListProps {
-  dateScope: GetPostRequestDto["dateScope"];
   sortScope: GetPostRequestDto["sortScope"];
   page: GetPostRequestDto["page"];
   maxContent: GetPostRequestDto["maxContent"];
   categoryName?: string;
-  isPaging?: boolean;
 }
 
 const MostPopularPostCardList = ({
-  dateScope,
   sortScope,
   page,
   maxContent,
   categoryName,
-  isPaging,
 }: MostPopularPostCardListProps) => {
+  const dateScope = useGetStringTypeToRouter(
+    "dateScope"
+  ) as GetPostRequestDto["dateScope"];
   const postList = useGetMostPopularPostListQuery({
-    dateScope,
+    dateScope: !dateScope ? "All" : dateScope,
     sortScope,
     page,
     maxContent,
@@ -37,35 +36,16 @@ const MostPopularPostCardList = ({
   if (postList.isLoading) {
     return <div>로딩중...</div>;
   }
-  if (!isPaging) {
-    return (
-      <>
-        <h3>{MOST_POPULAR_POSTCARD.ko}</h3>
-        <div className="grid grid-flow-row grid-cols-1 xl:grid-flow-col xl:grid-cols-none md:grid-cols-2 ">
-          {postList.data?.pages.map((postPage) =>
-            postPage.data.list.map((post, idx) => (
-              <MostPopularPostCard index={idx + 1} post={post} />
-            ))
-          )}
-        </div>
-      </>
-    );
-  }
+  if (!postList.data) return null;
+  if (isArrayEmpty(postList.data.data.list))
+    return <h3>인기 게시글이 없습니다.</h3>;
   return (
     <div>
-      <h4>{!categoryName ? "All" : categoryName}</h4>
-      <div className="grid grid-row gap-y-3">
-        {postList.data?.pages.map((postPage) =>
-          postPage.data.list.map((post, idx) => (
-            <MostPopularPostCard index={idx} post={post} />
-          ))
-        )}
+      <div className="grid grid-row md:grid-cols-2 xl:grid-cols-3 gap-y-3">
+        {postList.data.data.list.map((post, idx) => (
+          <MostPopularPostCard index={idx + 1} post={post} />
+        ))}
       </div>
-      <CardLoading
-        hasNextPage={postList.hasNextPage}
-        isFetchingNextPage={postList.isFetchingNextPage}
-        fetchNextPage={postList.fetchNextPage}
-      />
     </div>
   );
 };
