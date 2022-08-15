6c226333036ee137ca0d479543257646bc302b21
MINJE-98 <jmj012100@gmail.com>
2022-08-15 23:37:05 +0900

    feat(molecules-text-area): Link 기능 추가, Style 수정

--- src/components/common/molecules/text-area/hooks/useTiptapEditor.tsx
+++ src/components/common/molecules/text-area/hooks/useTiptapEditor.tsx
@@ -1,3 +1,4 @@
+import Link from "@tiptap/extension-link";
 import { EditorOptions, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
@@ -10,7 +11,7 @@ import { CreatePostRequestBodyDto } from "@til-log.lab/tilog-api";
 const useTiptapEditor = (content: EditorOptions["content"]) => {
   const { setValue } = useFormContext<CreatePostRequestBodyDto>();
   return useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, Link],
 
     editable: true,
     content,
@@ -21,7 +22,7 @@ const useTiptapEditor = (content: EditorOptions["content"]) => {
     editorProps: {
       attributes: {
         class:
-          "w-full h-full prose xl:prose-xl max-w-none dark:prose-invert focus:outline-none overflow-y-auto prose-img:ml-auto prose-img:mr-auto prose-img:border prose-img:border-gray-200 dark:text-white text-natural-700 dark:prose-img:border-gray-700",
+          "w-full h-full prose scrollbar-hide prose-p:text-neutral-800 dark:prose-p:text-neutral-50 max-w-none dark:prose-invert focus:outline-none overflow-y-auto prose-img:ml-auto prose-img:mr-auto prose-img:border prose-img:border-gray-200 dark:text-white text-natural-700 dark:prose-img:border-gray-700",
       },
     },
   });
