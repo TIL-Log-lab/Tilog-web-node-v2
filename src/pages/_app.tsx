a5eb6cb26eadd5cfaa4f0962af097c5723ae6a19
MINJE-98 <jmj012100@gmail.com>
2022-08-16 13:48:47 +0900

    feat(pages): _app 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import "../../styles/globals.css";
+import "../../styles/nprogress.css";
 
 import { AppProps } from "next/app";
 
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import Header from "@Components/common/organisms/header";
+import useProgressBar from "@Hooks/pages/app/useProgressBar";
+
 const queryClient = new QueryClient({});
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useProgressBar();
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
+      <Header />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
